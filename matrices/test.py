4d75d4c80473db2a3c2098baa5fe6d6ec6fe1624
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 13:04:04 -0500

    [transpose] Create function for switching row-column values

--- matrices/test.py
+++ matrices/test.py
@@ -1,6 +1,7 @@
 from addition import addition
 from multiplication import multiplication
 from scalar import scalar
+from transpose import transpose
 
 first_input_matrix = [
     [5, 8],
@@ -17,7 +18,9 @@ scalar_number = -7
 addition_output_matrix = addition(first_input_matrix, second_input_matrix)
 multiplication_output_matrix = multiplication(first_input_matrix, second_input_matrix)
 scalar_output_matrix = scalar(first_input_matrix, scalar_number)
+transpose_output_matrix = transpose(first_input_matrix)
 
 print(f'Addition: {addition_output_matrix}') # => [[9, 9], [9, 6]]
 print(f'Multiplication: {multiplication_output_matrix}') # => [[76, 29], [29, 11]]
 print(f'Scalar: {scalar_output_matrix}') # => [[-35, -56], [-14, -21]]
+print(f'Transpose: {transpose_output_matrix}') # => [[5, 2], [8, 3]]
--- matrices/transpose.py
+++ matrices/transpose.py
@@ -1,2 +1,10 @@
 def transpose(matrix):
-    pass
+    r1c1 = matrix[0][0]
+    r1c2 = matrix[1][0]
+    r2c1 = matrix[0][1]
+    r2c2 = matrix[1][1]
+    result = [
+        [r1c1, r1c2],
+        [r2c1, r2c2]
+    ]
+    return result
