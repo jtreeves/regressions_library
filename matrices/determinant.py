1e7fc1c50c43ba3c378e18b5e7a00b15106b1905
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 16:17:20 -0500

    [test] Move 3-D determinant test

--- matrices/determinant.py
+++ matrices/determinant.py
@@ -8,12 +8,3 @@ def determinant_3d(matrix):
     left_determinant = determinant([[matrix[1][0], matrix[1][1]], [matrix[2][0], matrix[2][1]]])
     result = matrix[0][0] * right_determinant - matrix[0][1] * spread_determinant + matrix[0][2] * left_determinant
     return result
-
-A = [
-    [6, 1, 1],
-    [4, -2, 5],
-    [2, 8, 7]
-]
-
-det_A = determinant_3d(A)
-print(det_A) # => -306
--- matrices/test.py
+++ matrices/test.py
@@ -2,7 +2,7 @@ from .addition import addition
 from .multiplication import multiplication
 from .scalar import scalar
 from .transpose import transpose
-from .determinant import determinant
+from .determinant import determinant, determinant_3d
 from .dot_product import dot_product
 from .columns import columns
 from .inverse import inverse
@@ -17,6 +17,12 @@ second_input_matrix = [
     [7, 3]
 ]
 
+first_3d_input_matrix = [
+    [6, 1, 1],
+    [4, -2, 5],
+    [2, 8, 7]
+]
+
 scalar_number = -7
 
 addition_output_matrix = addition(first_input_matrix, second_input_matrix)
@@ -26,6 +32,7 @@ transpose_output_matrix = transpose(first_input_matrix)
 determinant_output = determinant(first_input_matrix)
 dot_product_output = dot_product(first_input_matrix[0], columns(second_input_matrix)[0])
 inverse_output = inverse(first_input_matrix)
+determinant_output_3d = determinant_3d(first_3d_input_matrix)
 
 print(f'Addition: {addition_output_matrix}') # => [[9, 9], [9, 6]]
 print(f'Multiplication: {multiplication_output_matrix}') # => [[76, 29], [29, 11]]
@@ -34,3 +41,4 @@ print(f'Transpose: {transpose_output_matrix}') # => [[5, 2], [8, 3]]
 print(f'Determinant: {determinant_output}') # => -1
 print(f'Dot Product: {dot_product_output}') # => 76
 print(f'Inverse: {inverse_output}') # => [[-3, 8], [2, -5]]
+print(f'3-D Determinant: {determinant_output_3d}') # => -306
