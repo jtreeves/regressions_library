1c218c131d6da87f388195a8bbf49940a0d086f1
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 13:29:58 -0500

    [gitignore] Try to ignore pycache folders

---
+++ matrices/determinant.py
@@ -0,0 +1,3 @@
+def determinant(matrix):
+    result = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    return result
--- matrices/test.py
+++ matrices/test.py
@@ -2,6 +2,9 @@ from addition import addition
 from multiplication import multiplication
 from scalar import scalar
 from transpose import transpose
+from determinant import determinant
+from dot_product import dot_product
+from matrix import columns
 
 first_input_matrix = [
     [5, 8],
@@ -19,8 +22,12 @@ addition_output_matrix = addition(first_input_matrix, second_input_matrix)
 multiplication_output_matrix = multiplication(first_input_matrix, second_input_matrix)
 scalar_output_matrix = scalar(first_input_matrix, scalar_number)
 transpose_output_matrix = transpose(first_input_matrix)
+determinant_output = determinant(first_input_matrix)
+dot_product_output = dot_product(first_input_matrix[0], columns(second_input_matrix)[0])
 
 print(f'Addition: {addition_output_matrix}') # => [[9, 9], [9, 6]]
 print(f'Multiplication: {multiplication_output_matrix}') # => [[76, 29], [29, 11]]
 print(f'Scalar: {scalar_output_matrix}') # => [[-35, -56], [-14, -21]]
 print(f'Transpose: {transpose_output_matrix}') # => [[5, 2], [8, 3]]
+print(f'Determinant: {determinant_output}') # => -1
+print(f'Dot Product: {dot_product_output}') # => 76
