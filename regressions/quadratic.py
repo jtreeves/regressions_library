1757d9b95b3e95be61fd38208fb32dbc6f457914
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 15:57:12 -0500

    [quadratic] Copy and tweak linear code

---
+++ regressions/quadratic.py
@@ -0,0 +1,21 @@
+from matrices.multiplication import multiplication, multiplication_vector
+from matrices.transpose import transpose
+from matrices.inverse import inverse
+
+def quadratic(data):
+    independent_matrix = [
+        [data[0][0]**2, data[0][0], 1],
+        [data[1][0]**2, data[1][0], 1],
+        [data[2][0]**2, data[2][0], 1]
+    ]
+    dependent_matrix = [
+        [data[0][1]],
+        [data[1][1]],
+        [data[2][1]]
+    ]
+    transposition = transpose(independent_matrix)
+    product = multiplication(transposition, independent_matrix)
+    inversion = inverse(product)
+    second_product = multiplication(inversion, transposition)
+    result = multiplication_vector(second_product, dependent_matrix)
+    return result
