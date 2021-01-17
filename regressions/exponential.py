7ba50ddf0d41f1d49ea678d4f9f759d8328d73f6
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 17:45:01 -0500

    [exponential] Import log and exp, and tweak linearization

--- regressions/exponential.py
+++ regressions/exponential.py
@@ -1,3 +1,5 @@
+from math import log, exp
+
 from matrices.multiplication import multiplication, multiplication_vector
 from matrices.transpose import transpose
 from matrices.inverse import inverse
@@ -8,12 +10,17 @@ def exponential(data):
         [data[1][0], 1]
     ]
     dependent_matrix = [
-        [data[0][1]],
-        [data[1][1]]
+        [log(data[0][1])],
+        [log(data[1][1])]
     ]
     transposition = transpose(independent_matrix)
     product = multiplication(transposition, independent_matrix)
     inversion = inverse(product)
     second_product = multiplication(inversion, transposition)
-    result = multiplication_vector(second_product, dependent_matrix)
+    solution = multiplication_vector(second_product, dependent_matrix)
+    result = [
+        [exp(solution[1][0])],
+        [solution[0][0]]
+    ]
     return result
+
