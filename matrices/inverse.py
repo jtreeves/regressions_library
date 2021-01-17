76d06c41d2189f0e33ea04861ba263fca2f66b94
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 14:21:22 -0500

    [inverse] Create function

--- matrices/inverse.py
+++ matrices/inverse.py
@@ -2,4 +2,7 @@ from scalar import scalar
 from determinant import determinant
 
 def inverse(matrix):
-    pass
+    determinant_reciprocal = 1 / determinant(matrix)
+    transform = [[matrix[1][1], -1 * matrix[0][1]], [-1 * matrix[1][0], matrix[0][0]]]
+    result = scalar(transform, determinant_reciprocal)
+    return result
--- matrices/test.py
+++ matrices/test.py
@@ -5,6 +5,7 @@ from transpose import transpose
 from determinant import determinant
 from dot_product import dot_product
 from columns import columns
+from inverse import inverse
 
 first_input_matrix = [
     [5, 8],
@@ -24,6 +25,7 @@ scalar_output_matrix = scalar(first_input_matrix, scalar_number)
 transpose_output_matrix = transpose(first_input_matrix)
 determinant_output = determinant(first_input_matrix)
 dot_product_output = dot_product(first_input_matrix[0], columns(second_input_matrix)[0])
+inverse_output = inverse(first_input_matrix)
 
 print(f'Addition: {addition_output_matrix}') # => [[9, 9], [9, 6]]
 print(f'Multiplication: {multiplication_output_matrix}') # => [[76, 29], [29, 11]]
@@ -31,3 +33,4 @@ print(f'Scalar: {scalar_output_matrix}') # => [[-35, -56], [-14, -21]]
 print(f'Transpose: {transpose_output_matrix}') # => [[5, 2], [8, 3]]
 print(f'Determinant: {determinant_output}') # => -1
 print(f'Dot Product: {dot_product_output}') # => 76
+print(f'Inverse: {inverse_output}') # => [[-3, 8], [2, -5]]
