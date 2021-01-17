befaf0ff04d47df9202801cee44c7c61e6e3e7f7
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 17:30:12 -0500

    [test] Move quadratic test case

--- regressions/test.py
+++ regressions/test.py
@@ -1,7 +1,11 @@
 from .linear import linear
+from .quadratic import quadratic
 
 linear_set = [[2, 5], [7, 6]]
+quadratic_set = [[1, 4], [2, 9], [3, 16]]
 
 linear_solution = linear(linear_set)
+quadratic_solution = quadratic(quadratic_set)
 
 print(f'Linear: {linear_solution}') # => [[0.2], [4.6]]
+print(f'Quadratic: {quadratic_solution}') # => [[1.0], [2.0], [1.0]]
