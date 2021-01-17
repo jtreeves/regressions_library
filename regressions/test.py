a3175ba30fec8466b0373afca3e0197d6cadee4f
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 18:15:25 -0500

    [test] Add print statements for the specific functions

--- regressions/test.py
+++ regressions/test.py
@@ -13,7 +13,11 @@ quadratic_solution = quadratic(quadratic_set)
 exponential_solution = exponential(exponential_set)
 logarithmic_solution = logarithmic(logarithmic_set)
 
-print(f'Linear: {linear_solution}') # => [[0.2], [4.6]]
-print(f'Quadratic: {quadratic_solution}') # => [[1.0], [2.0], [1.0]]
-print(f'Exponential: {exponential_solution}') # => [[3.0], [2.0]]
-print(f'Logarithmic: {logarithmic_solution}') # => [[5.0], [6.8268]]
+print(f'Linear Matrix: {linear_solution}') # => [[0.2], [4.6]]
+print(f'Linear Equation: y = {linear_solution[0][0]}x + {linear_solution[1][0]}') # => y = 0.2x + 4.6
+print(f'Quadratic Matrix: {quadratic_solution}') # => [[1.0], [2.0], [1.0]]
+print(f'Quadratic Equation: y = {quadratic_solution[0][0]}x^2 + {quadratic_solution[1][0]}x + {quadratic_solution[2][0]}') # => y = 1.0x^2 + 2.0x + 1.0
+print(f'Exponential Matrix: {exponential_solution}') # => [[3.0], [2.0]]
+print(f'Exponential Equation: y = {exponential_solution[0][0]}*{exponential_solution[1][0]}^x') # => y = 3.0*2.0^x
+print(f'Logarithmic Matrix: {logarithmic_solution}') # => [[5.0], [6.8268]]
+print(f'Logarithmic Equation: y = {logarithmic_solution[0][0]} + {logarithmic_solution[1][0]}*lnx') # => y = 5.0 + 6.8268*lnx
