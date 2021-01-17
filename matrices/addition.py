6bbed94ddd3edcddd3595639e13de4c1accc9690
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 18:49:55 -0500

    [addition] Refactor with each for loop getting one append action to display result as matrix instead of single list

--- matrices/addition.py
+++ matrices/addition.py
@@ -12,8 +12,9 @@ def addition(first_matrix, second_matrix):
 def addition_all(first_matrix, second_matrix):
     result = []
     for m in range(len(first_matrix)):
+        result.append([])
         for n in range(len(first_matrix[0])):
-            result.append(first_matrix[m][n] + second_matrix[m][n])
+            result[m].append(first_matrix[m][n] + second_matrix[m][n])
     return result
 
 A = [
