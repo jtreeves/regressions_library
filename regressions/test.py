92c935e5092e779a1b3fde34335ab05b90b45cbd
jtreeves <jackson.t.reeves@gmail.com>
2021-01-18 16:14:41 -0500

    [test] Add case for quadratic with 5 input pairs

--- regressions/test.py
+++ regressions/test.py
@@ -10,6 +10,7 @@ linear_set = [[2, 5], [7, 6]]
 linear_set_3 = [[1, 5], [2, 7], [3, 9]]
 linear_set_10 = [[1, 2], [2, 1], [3, 4], [4, 6], [5, 5], [6, 8], [7, 8], [8, 7], [9, 11], [10, 14]]
 quadratic_set = [[1, 4], [2, 9], [3, 16]]
+quadratic_set_5 = [[1, 5], [2, 12], [3, 34], [4, 23], [5, 2]]
 quadratic_set_10 = [[83, 183], [71, 168], [64, 171], [69, 178], [69, 176], [64, 172], [68, 165], [59, 158], [81, 183], [91, 182]]
 cubic_set = [[1, 6], [2, 16], [3, 58], [4, 150]]
 cubic_set_10 = [[1, 3], [2, 29], [3, 40], [4, 73], [5, 56], [6, 22], [7, 10], [8, 17], [9, 51], [10, 89]]
@@ -23,6 +24,7 @@ linear_solution_3 = linear(linear_set_3)
 linear_solution_10 = linear(linear_set_10)
 linear_solution_agnostic = linear(agnostic_set)
 quadratic_solution = quadratic(quadratic_set)
+quadratic_solution_5 = quadratic(quadratic_set_5)
 quadratic_solution_10 = quadratic(quadratic_set_10)
 quadratic_solution_agnostic = quadratic(agnostic_set)
 cubic_solution = cubic(cubic_set)
@@ -46,6 +48,8 @@ print(f'Linear Matrix Agnostic: {linear_solution_agnostic}') # => [[79.7212], [�
 print(f'Linear Equation Agnostic: y = {linear_solution_agnostic[0][0]}x + {linear_solution_agnostic[1][0]}') # => y = 79.7212x + −7.0667
 print(f'Quadratic Matrix: {quadratic_solution}') # => [[1.0], [2.0], [1.0]]
 print(f'Quadratic Equation: y = {quadratic_solution[0][0]}x^2 + {quadratic_solution[1][0]}x + {quadratic_solution[2][0]}') # => y = 1.0x^2 + 2.0x + 1.0
+print(f'Quadratic Matrix 5: {quadratic_solution_5}') # => [[−6.3571], [38.6429], [−30.8000]]
+print(f'Quadratic Equation 5: y = {quadratic_solution_5[0][0]}x^2 + {quadratic_solution_5[1][0]}x + {quadratic_solution_5[2][0]}') # => y = −6.3571x^2 + 38.6429x + −30.8000
 print(f'Quadratic Matrix 10: {quadratic_solution_10}') # => [[-18.3907], [4.5614], [-0.0259]]
 print(f'Quadratic Equation 10: y = {quadratic_solution_10[0][0]}x^2 + {quadratic_solution_10[1][0]}x + {quadratic_solution_10[2][0]}') # => y = -18.3907x^2 + 4.5614x + -0.0259
 print(f'Quadratic Matrix Agnostic: {quadratic_solution_agnostic}') # => [[−1.6515], [97.8879], [−43.4000]]
