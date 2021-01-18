b695714f382f21f4f215ef10b5b8dcd433f7c221
jtreeves <jackson.t.reeves@gmail.com>
2021-01-18 16:37:31 -0500

    [error] Add function for b-Ax, not magnitude squared of Ax-b

--- regressions/error.py
+++ regressions/error.py
@@ -3,6 +3,7 @@ from matrices.scalar import scalar
 from matrices.multiplication import multiplication
 
 def error(independent, dependent, solution):
-    product = multiplication()
-    result = addition()
+    product = multiplication(independent, solution)
+    negation = scalar(product, -1)
+    result = addition(dependent, negation)
     return result
