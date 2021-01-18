3793577c4f1982c2925629d40efa9741a8e66c14
jtreeves <jackson.t.reeves@gmail.com>
2021-01-18 14:07:00 -0500

    [test] Add case for 10 inputs for linear regression

--- regressions/test.py
+++ regressions/test.py
@@ -7,6 +7,7 @@ from .logarithmic import logarithmic
 
 linear_set = [[2, 5], [7, 6]]
 linear_set_3 = [[1, 5], [2, 7], [3, 9]]
+linear_set_10 = [[1, 2], [2, 1], [3, 4], [4, 6], [5, 5], [6, 8], [7, 8], [8, 7], [9, 11], [10, 14]]
 quadratic_set = [[1, 4], [2, 9], [3, 16]]
 cubic_set = [[1, 6], [2, 16], [3, 58], [4, 150]]
 rational_set = [[2, 2], [3, 1]]
@@ -15,6 +16,7 @@ logarithmic_set = [[1, 5], [9, 20]]
 
 linear_solution = linear(linear_set)
 linear_solution_3 = linear(linear_set_3)
+linear_solution_10 = linear(linear_set_10)
 quadratic_solution = quadratic(quadratic_set)
 cubic_solution = cubic(cubic_set)
 rational_solution = rational(rational_set)
@@ -25,6 +27,8 @@ print(f'Linear Matrix: {linear_solution}') # => [[0.2], [4.6]]
 print(f'Linear Equation: y = {linear_solution[0][0]}x + {linear_solution[1][0]}') # => y = 0.2x + 4.6
 print(f'Linear Matrix 3: {linear_solution_3}') # => [[2.0], [3.0]]
 print(f'Linear Equation 3: y = {linear_solution_3[0][0]}x + {linear_solution_3[1][0]}') # => y = 2.0x + 3.0
+print(f'Linear Matrix 10: {linear_solution_10}') # => [[1.2242], [-0.1333]]
+print(f'Linear Equation 10: y = {linear_solution_10[0][0]}x + {linear_solution_10[1][0]}') # => y = 1.2242x + -0.1333
 print(f'Quadratic Matrix: {quadratic_solution}') # => [[1.0], [2.0], [1.0]]
 print(f'Quadratic Equation: y = {quadratic_solution[0][0]}x^2 + {quadratic_solution[1][0]}x + {quadratic_solution[2][0]}') # => y = 1.0x^2 + 2.0x + 1.0
 print(f'Cubic Matrix: {cubic_solution}') # => [[3.0], [-2.0], [-5.0], [10.0]]
