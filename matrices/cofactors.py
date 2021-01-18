6f8c8d7f8f8dd40cf5e3fe62455971862b663389
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 21:13:14 -0500

    [cofactors] Build out generalized form with double for and if loops

--- matrices/cofactors.py
+++ matrices/cofactors.py
@@ -18,10 +18,17 @@ def cofactors(matrix):
 def cofactors_all(matrix):
     result = []
     for m in range(len(matrix)):
+        result.append([])
         if m % 2 == 0:
-            # every other column is negative, starting with 2nd
-            pass
+            for n in range(len(matrix[0])):
+                if n % 2 == 0:
+                    result[m].append(matrix[m][n])
+                else:
+                    result[m].append(-1 * matrix[m][n])
         else:
-            # every other column is negative, starting with 1st
-            pass
+            for n in range(len(matrix[0])):
+                if n % 2 == 0:
+                    result[m].append(-1 * matrix[m][n])
+                else:
+                    result[m].append(matrix[m][n])
     return result
