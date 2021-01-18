3073a405688825e162b250aea868c58c188191a8
jtreeves <jackson.t.reeves@gmail.com>
2021-01-17 19:22:17 -0500

    [columns] Swap m and n to fix

--- matrices/columns.py
+++ matrices/columns.py
@@ -27,5 +27,5 @@ def columns_all(matrix):
     for m in range(len(matrix)):
         result.append([])
         for n in range(len(matrix[0])):
-            result[m].append(matrix[m][n])
+            result[m].append(matrix[n][m])
     return result
