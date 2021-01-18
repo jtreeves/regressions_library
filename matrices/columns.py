1021f24d689c2ba3198269cd4a4c4fe85802a963
jtreeves <jackson.t.reeves@gmail.com>
2021-01-18 13:52:58 -0500

    [columns] Make columns identical to new transpose

--- matrices/columns.py
+++ matrices/columns.py
@@ -1,8 +1,8 @@
 def columns(matrix):
     result = []
-    for m in range(len(matrix)):
+    for m in range(len(matrix[0])):
         result.append([])
-        for n in range(len(matrix[0])):
+        for n in range(len(matrix)):
             result[m].append(matrix[n][m])
     return result
 
