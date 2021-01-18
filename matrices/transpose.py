633bb45001e86b824621bbc2a5eb1366ecf7d2c1
jtreeves <jackson.t.reeves@gmail.com>
2021-01-18 13:44:15 -0500

    [transpose] Refactor with attention to switching rows and columns

--- matrices/transpose.py
+++ matrices/transpose.py
@@ -8,17 +8,10 @@ def transpose(matrix):
 
 def transpose_all(matrix):
     result = []
-    return result
-
-def diminished(matrix, row, column):
-    result = []
-    storage = {}
-    for m in range(len(matrix)):
-        if m != row:
-            storage[m] = []
-            for n in range(len(matrix[0])):
-                if n!= column:
-                    storage[m].append(matrix[m][n])
-    for key in storage:
-        result.append(storage[key])
+    rows = len(matrix[0])
+    columns = len(matrix)
+    for row in range(rows):
+        result.append([])
+        for column in range(columns):
+            result[row].append(matrix[column][row])
     return result
