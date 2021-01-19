5a00a33877edfd2d45f13dca60817909f54ff720
jtreeves <jackson.t.reeves@gmail.com>
2021-01-19 03:59:33 -0500

    [sinusoidal] Add line breaks within constant definitions to make easier to understand

--- regressions/sinusoidal.py
+++ regressions/sinusoidal.py
@@ -33,9 +33,28 @@ def sinusoidal(data):
     print(f'SINUSOIDAL Second Product: {second_product}')
     solution = multiplication(second_product, dependent_matrix)
     print(f'SINUSOIDAL Solution: {solution}')
-    constant_b = (((362880 * solution[0][0] / solution[8][0])**(1/8)) + (((362880 * solution[0][0]) / (120 * solution[4][0]))**(1/4)) + (((120 * solution[4][0]) / (solution[8][0]))**(1/4)) + (((40320 * solution[1][0]) / (24 * solution[5][0]))**(1/4)) + (((5040 * solution[2][0]) / (6 * solution[6][0]))**(1/4)) + (((720 * solution[3][0]) / (2 * solution[7][0]))**(1/4))) / 6
-    constant_c = atan((-6720 * solution[1][0]) / ((constant_b**5) * solution[6][0]))
-    constant_a = ((362880 * solution[0][0]) / ((constant_b**9) * cos(constant_c)) + (40320 * solution[1][0]) / ((constant_b**8) * cos(constant_c)) + (-5040 * solution[2][0]) / ((constant_b**7) * cos(constant_c)) + (-720 * solution[3][0]) / ((constant_b**6) * cos(constant_c)) + (120 * solution[4][0]) / ((constant_b**5) * cos(constant_c)) + (24 * solution[5][0]) / ((constant_b**4) * cos(constant_c)) + (-6 * solution[6][0]) / ((constant_b**3) * cos(constant_c)) + (-2 * solution[7][0]) / ((constant_b**2) * cos(constant_c)) + (solution[8][0]) / ((constant_b) * cos(constant_c))) / 9
+    constant_b = (
+        ((362880 * solution[0][0] / solution[8][0])**(1/8)) + 
+        (((362880 * solution[0][0]) / (120 * solution[4][0]))**(1/4)) + 
+        (((120 * solution[4][0]) / (solution[8][0]))**(1/4)) + 
+        (((40320 * solution[1][0]) / (24 * solution[5][0]))**(1/4)) + 
+        (((5040 * solution[2][0]) / (6 * solution[6][0]))**(1/4)) + 
+        (((720 * solution[3][0]) / (2 * solution[7][0]))**(1/4))
+    ) / 6
+    constant_c = (
+        atan((-6720 * solution[1][0]) / ((constant_b**5) * solution[6][0]))
+    ) / 20
+    constant_a = (
+        (362880 * solution[0][0]) / ((constant_b**9) * cos(constant_c)) + 
+        (40320 * solution[1][0]) / ((constant_b**8) * cos(constant_c)) + 
+        (-5040 * solution[2][0]) / ((constant_b**7) * cos(constant_c)) + 
+        (-720 * solution[3][0]) / ((constant_b**6) * cos(constant_c)) + 
+        (120 * solution[4][0]) / ((constant_b**5) * cos(constant_c)) + 
+        (24 * solution[5][0]) / ((constant_b**4) * cos(constant_c)) + 
+        (-6 * solution[6][0]) / ((constant_b**3) * cos(constant_c)) + 
+        (-2 * solution[7][0]) / ((constant_b**2) * cos(constant_c)) + 
+        (solution[8][0]) / ((constant_b) * cos(constant_c))
+    ) / 9
     constant_d = solution[9][0] - (constant_a * sin(constant_c))
     constants = [
         [constant_a],
