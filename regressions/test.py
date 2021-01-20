2d677bba779ed98ffe222473c23a7920d1eed33b
jtreeves <jackson.t.reeves@gmail.com>
2021-01-19 22:40:36 -0500

    [test] Add specifically sinusoidal set

--- regressions/test.py
+++ regressions/test.py
@@ -2,7 +2,10 @@ from .run_all import run_all
 
 set_20 = [[1, 78], [2, 64], [3, 97], [4, 34], [5, 53], [6, 65], [7, 79], [8, 201], [9, 123], [10, 145], [11, 356], [12, 77], [13, 478], [14, 598], [15, 376], [16, 294], [17, 401], [18, 532], [19, 607], [20, 752]]
 
+sinusoidal_set = [[1, 7], [2, 12], [3, 19], [4, 25], [5, 32], [6, 28], [7, 21], [8, 14], [9, 9], [10, 6], [11, 10], [12, 17], [13, 18], [14, 25], [15, 31], [16, 35], [17, 26], [18, 22], [19, 15], [20, 8]]
+
 all_solutions = run_all(set_20)
+sinusoidal_solutions = run_all(sinusoidal_set)
 
 print(f"Linear Constants: {all_solutions['options']['linear']['constants']}") # => [[32.98796992481202], [-75.87368421052619]]
 print(f"Linear Error: {all_solutions['options']['linear']['error']}") # => 22.312581356511483
@@ -27,6 +30,9 @@ print(f"Sinusoidal Error: {all_solutions['options']['sinusoidal']['error']}") # 
 
 print(f"Best Choice Function: {all_solutions['optimal']['function']}") # => cubic
 print(f"Best Choice Error: {all_solutions['optimal']['error']}") # => 21.05493315315674
+
+print(f"Sinusoidal Constants from Sinusoidal Set: {sinusoidal_solutions['options']['sinusoidal']['constants']}") # => [[-10637.7607716477], [1.041350832607493], [1.0384915217851285], [11620.027599096207]]
+print(f"Sinusoidal Error from Sinusoidal Set: {sinusoidal_solutions['options']['sinusoidal']['error']}") # => 242.7427759867437
 
 # agnostic_set = [[1, 3], [2, 147], [3, 286], [4, 352], [5, 423], [6, 510], [7, 591], [8, 451], [9, 689], [10, 862]]
 # # cyclic_set = [[1, 33], [2, 27], [3, 45], [4, 56], [5, 69], [6, 84], [7, 88], [8, 75], [9, 57], [10, 42], [11, 39], [12, 28]]
