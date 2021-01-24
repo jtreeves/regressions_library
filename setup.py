f0dd5ca3f28c93ec7ca124c5992a8e9d472f8a05
jtreeves <jackson.t.reeves@gmail.com>
2021-01-24 17:00:31 -0500

    Add files for publishing to PyPI

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='regressions',
     version='1.0',
     url='https://github.com/jtreeves/regressions_library',
-    license='CC0',
+    license='MIT',
     author='Jackson Reeves',
     author_email='jr@jacksonreeves.com',
     description='Generate regression models from data',
     packages=find_packages(exclude=['tests']),
     long_description=open('README.md').read(),
     zip_safe=False,
-    setup_requires=['nose>=1.0'],
+    setup_requires=['nose>=1.0', 'numpy'],
     test_suite='nose.collector'
 )
