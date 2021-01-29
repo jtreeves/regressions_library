67ada1075843899a319ff980bcb0fd744ce4e0e8
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 13:37:02 -0500

    [derivatives] Build function to generate cubic derivatives

---
+++ library/analyses/derivatives/cubic.py
@@ -0,0 +1,5 @@
+def cubic(first_constant, second_constant, third_constant):
+    def cubic_derivative(variable):
+        evaluation = 3 * first_constant * variable**2 + 2 * second_constant * variable + third_constant
+        return evaluation
+    return cubic_derivative
