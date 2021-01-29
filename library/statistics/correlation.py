42923c4f6e56a10c966974e2aadbc1da74f3b596
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 15:26:44 -0500

    [correlation] Add function to determine correlation of data set

---
+++ library/statistics/correlation.py
@@ -0,0 +1,17 @@
+from .residual import residual_array as residuals
+from .deviation import deviation_array as deviations
+from library.sequences.summation import summation
+
+def correlation(actuals, expecteds):
+    residual_array = residuals(actuals, expecteds)
+    deviation_array = deviations(actuals)
+    squared_residuals = []
+    for i in range(len(residual_array)):
+        squared_residuals.append(residual_array[i]**2)
+    squared_deviations = []
+    for i in range(len(deviation_array)):
+        squared_deviations.append(deviation_array[i]**2)
+    residual_sum = summation(squared_residuals)
+    deviation_sum = summation(squared_deviations)
+    result = (1 - residual_sum / deviation_sum)**(1/2)
+    return result
--- library/statistics/deviation.py
+++ library/statistics/deviation.py
@@ -1,9 +1,12 @@
+from .mean import mean
+
 def deviation(actual, mean): 
     result = actual - mean
     return result
 
-def deviation_array(actual_array, expected_array):
+def deviation_array(actual_array):
     results = []
+    average = mean(actual_array)
     for i in range(len(actual_array)):
-        results.append(deviation(actual_array[i], expected_array[i]))
+        results.append(deviation(actual_array[i], average))
     return results
