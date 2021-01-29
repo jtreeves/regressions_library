56fd6a67f19c136028796793af85a2ee0934d1ca
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 16:42:43 -0500

    [range] Refactor to handle an array as input

--- library/statistics/range.py
+++ library/statistics/range.py
@@ -1,3 +1,8 @@
-def range(maximum, minimum):
-    result = maximum - minimum
+from .maximum import maximum
+from .minimum import minimum
+
+def range(data):
+    max_value = maximum(data)
+    min_value = minimum(data)
+    result = max_value - min_value
     return result
