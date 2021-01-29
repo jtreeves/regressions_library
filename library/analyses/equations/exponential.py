fac9efc2f0aee9b7141412aa3c949243302f9af8
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 14:01:25 -0500

    [integrals] Build function to generate exponential integrals

--- library/analyses/equations/exponential.py
+++ library/analyses/equations/exponential.py
@@ -1,7 +1,5 @@
-from math import log
-
 def exponential(first_constant, second_constant):
     def exponential_equation(variable):
-        evaluation = first_constant * second_constant**variable * log(second_constant)
+        evaluation = first_constant * second_constant**variable
         return evaluation
-    return exponential_equation
+    return exponential_equation 
---
+++ library/analyses/integrals/exponential.py
@@ -0,0 +1,7 @@
+from math import log
+
+def exponential(first_constant, second_constant):
+    def exponential_integral(variable):
+        evaluation = first_constant * second_constant**variable / log(second_constant)
+        return evaluation
+    return exponential_integral
