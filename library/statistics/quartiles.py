20e9b6e78a9cd51a06928fc0cbcf12b0d211bb81
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 17:25:26 -0500

    [quartiles] Create function to return the various quartiles of a data set

---
+++ library/statistics/quartiles.py
@@ -0,0 +1,34 @@
+from .median import median
+from .sort import sort
+from math import floor
+
+def halve(data):
+    sorted_data = sort(data)
+    length = len(sorted_data)
+    upper = []
+    lower = []
+    if len % 2 == 0:
+        upper_index = length / 2
+        lower_index = upper_index - 1
+        upper = data[upper_index:]
+        lower = data[:lower_index]
+    else:
+        index = floor(length / 2)
+        upper = data[index:]
+        lower = data[:index]
+    result = {
+        'upper': upper,
+        'lower': lower
+    }
+    return result
+
+def quartiles(data, q):
+    halved_data = halve(data)
+    result = ''
+    if q == 2:
+        result = median(data)
+    elif q == 1:
+        result = median(halved_data['lower'])
+    elif q == 3:
+        result = median(halved_data['upper'])
+    return result
