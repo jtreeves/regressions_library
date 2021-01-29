674505b0e89df68afc975be95a2a37af700ab1c0
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 13:44:03 -0500

    [derivatives] Build function to generate exponential derivatives

--- library/analyses/equations/exponential.py
+++ library/analyses/equations/exponential.py
@@ -1,5 +1,7 @@
+from math import log
+
 def exponential(first_constant, second_constant):
     def exponential_equation(variable):
-        evaluation = first_constant * second_constant**variable
+        evaluation = first_constant * second_constant**variable * log(second_constant)
         return evaluation
     return exponential_equation
