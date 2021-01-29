529c6148f252376199afab161c7d4511e3f1f8c1
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 13:40:26 -0500

    [derivatives] Build function to generate hyperbolic derivatives

---
+++ library/analyses/derivatives/hyperbolic.py
@@ -0,0 +1,5 @@
+def hyperbolic(first_constant):
+    def hyperbolic_derivative(variable):
+        evaluation = -1 * first_constant / variable**2
+        return evaluation
+    return hyperbolic_derivative
