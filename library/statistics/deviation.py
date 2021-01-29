294bc28f636a22d55f279c98a7d83b81ec0928ce
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 15:02:35 -0500

    [statistics] Add array versions for residual and deviation functions

--- library/statistics/deviation.py
+++ library/statistics/deviation.py
@@ -1,2 +1,9 @@
-def residual(actual, mean):
-    return actual - mean
+def deviation(actual, mean): 
+    result = actual - mean
+    return result
+
+def deviation_array(actual_array, expected_array):
+    results = []
+    for i in range(len(actual_array)):
+        results.append(deviation(actual_array[i], expected_array[i]))
+    return results
--- library/statistics/residual.py
+++ library/statistics/residual.py
@@ -1,2 +1,9 @@
 def residual(actual, expected):
-    return actual - expected
+    result = actual - expected
+    return result
+
+def residual_array(actual_array, expected_array):
+    results = []
+    for i in range(len(actual_array)):
+        results.append(residual(actual_array[i], expected_array[i]))
+    return results
