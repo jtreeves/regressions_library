95d5978d870cd74c3c0c071add7ffdab5531a4d0
jtreeves <jackson.t.reeves@gmail.com>
2021-01-29 23:03:01 -0500

    [critical_points] Fix function to generate array of points for different derivatives

--- library/analyses/critical_points.py
+++ library/analyses/critical_points.py
@@ -1,15 +1,7 @@
 from .roots.linear import linear as linroots
 from .roots.quadratic import quadratic as quadroots
-from .roots.cubic import cubic as cubroots
-from .roots.hyperbolic import hyperbolic as hyproots
-from .roots.exponential import exponential as exproots
-from .roots.logarithmic import logarithmic as logroots
-from .derivatives.linear import linear as linder
 from .derivatives.quadratic import quadratic as quader
 from .derivatives.cubic import cubic as cubder
-from .derivatives.hyperbolic import hyperbolic as hypder
-from .derivatives.exponential import exponential as expder
-from .derivatives.logarithmic import logarithmic as logder
 
 def critical_points(equation_type, derivative_level, coefficients):
     results = []
@@ -19,13 +11,24 @@ def critical_points(equation_type, derivative_level, coefficients):
         elif equation_type == 'quadratic':
             results.append(linroots(quader(coefficients[0], coefficients[1], coefficients[2])['first']['constants']))
         elif equation_type == 'cubic':
-            results.append(quadroots())
+            results.append(quadroots(cubder(coefficients[0], coefficients[1], coefficients[2], coefficients[3])['first']['constants']))
         elif equation_type == 'hyperbolic':
             results.append(None)
         elif equation_type == 'exponential':
-            results.append(exproots(coefficients[0], coefficients[1]))
+            results.append(None)
         elif equation_type == 'logarithmic':
-            results.append(hyproots(coefficients[0], coefficients[1]))
+            results.append(None)
     elif derivative_level == 2:
-        return
+        if equation_type == 'linear':
+            results.append(None)
+        elif equation_type == 'quadratic':
+            results.append(None)
+        elif equation_type == 'cubic':
+            results.append(linroots(cubder(coefficients[0], coefficients[1], coefficients[2], coefficients[3])['second']['constants']))
+        elif equation_type == 'hyperbolic':
+            results.append(None)
+        elif equation_type == 'exponential':
+            results.append(None)
+        elif equation_type == 'logarithmic':
+            results.append(None)
     return results
