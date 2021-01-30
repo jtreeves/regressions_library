dbe7568eb18c6fa4adbef84be663a8a52f4183e0
jtreeves <jackson.t.reeves@gmail.com>
2021-01-30 12:35:37 -0500

    [maxima] Build function to determine if a point is a maximum by using intervals

---
+++ library/analyses/maxima.py
@@ -0,0 +1,6 @@
+def maxima(intervals):
+    result = []
+    for i in range(len(intervals)):
+        if intervals[i] == True and intervals[i + 2] == False:
+            result.append(intervals[i + 1])
+    return result
