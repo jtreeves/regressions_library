cbdc604dee16b87dd1d2de6c2be6af1d7caf4583
jtreeves <jackson.t.reeves@gmail.com>
2021-01-30 13:50:37 -0500

    [median] Fix by using brackets instead of parentheses to access instead of call, and using int on float intermediary result to use it as index

--- library/statistics/median.py
+++ library/statistics/median.py
@@ -7,17 +7,17 @@ def median(data):
     length = len(sorted_data)
     print(f'LENGTH: {length}')
     if length % 2 == 0:
-        upper_index = length / 2
+        upper_index = int(length / 2)
         print(f'UPPER_INDEX: {upper_index}')
-        lower_index = upper_index - 1
+        lower_index = int(upper_index - 1)
         print(f'LOWER_INDEX: {lower_index}')
-        upper_value = sorted_data(upper_index)
+        upper_value = sorted_data[upper_index]
         print(f'UPPER_VALUE: {upper_value}')
-        lower_value = sorted_data(lower_index)
+        lower_value = sorted_data[lower_index]
         print(f'LOWER_VALUE: {lower_value}')
         result = (upper_value + lower_value) / 2
         return result
     else:
-        index = floor(length / 2)
-        result = sorted_data(index)
+        index = int(floor(length / 2))
+        result = sorted_data[index]
         return result
