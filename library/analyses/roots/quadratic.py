35c349393dca0b6be268fad2833c0d8c3f4a5007
jtreeves <jackson.t.reeves@gmail.com>
2021-01-31 00:24:12 -0500

    [roots] Refactor quadratic to break up solutions onto separate lines

--- library/analyses/roots/quadratic.py
+++ library/analyses/roots/quadratic.py
@@ -1,4 +1,6 @@
 def quadratic(first_constant, second_constant, third_constant):
     discriminant = second_constant**2 - 4 * first_constant * third_constant
-    roots = [(-1 * second_constant + discriminant**(1/2))/(2 * first_constant), (-1 * second_constant - discriminant**(1/2))/(2 * first_constant)]
+    first_root = (-1 * second_constant + discriminant**(1/2)) / (2 * first_constant)
+    second_root = (-1 * second_constant - discriminant**(1/2)) / (2 * first_constant)
+    roots = [first_root, second_root]
     return roots
