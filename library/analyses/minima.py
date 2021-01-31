95c22c767c069864ba51b85895af6882bdc6ecb1
jtreeves <jackson.t.reeves@gmail.com>
2021-01-30 20:49:21 -0500

    [minima] Add maxima's fixes preemptively

--- library/analyses/minima.py
+++ library/analyses/minima.py
@@ -1,6 +1,11 @@
 def minima(intervals):
     result = []
     for i in range(len(intervals)):
-        if intervals[i] == 'decreasing' and intervals[i + 2] == 'increasing':
-            result.append(intervals[i + 1])
+        try:
+            if intervals[i] == 'decreasing' and intervals[i + 2] == 'increasing':
+                result.append(intervals[i + 1])
+        except IndexError:
+            pass
+    if len(result) == 0:
+        result.append(None)
     return result
