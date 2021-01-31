edfb752f0afeaad3751ed00d56ec732cab42817b
jtreeves <jackson.t.reeves@gmail.com>
2021-01-31 01:15:48 -0500

    [cubic, quadratic] Reconcile real and imaginary options

--- library/analyses/roots/cubic.py
+++ library/analyses/roots/cubic.py
@@ -1,4 +1,5 @@
 def cubic(first_constant, second_constant, third_constant, fourth_constant):
+    roots = []
     xi = (-1 + (-3)**(1/2)) / 2
     delta_first = second_constant**2 - 3 * first_constant * third_constant
     delta_second = 2 * second_constant**3 - 9 * first_constant * second_constant * third_constant + 27 * first_constant**2 * fourth_constant
@@ -24,9 +25,11 @@ def cubic(first_constant, second_constant, third_constant, fourth_constant):
     size_third_image = (third_imag**2)**(1/2)
     if size_first_image < 0.0001:
         first_root = first_real
+        roots.append(first_root)
     if size_second_image < 0.0001:
         second_root = second_real
+        roots.append(second_root)
     if size_third_image < 0.0001:
         third_root = third_real
-    roots = [first_root, second_root, third_root]
+        roots.append(third_root)
     return roots
--- library/analyses/roots/quadratic.py
+++ library/analyses/roots/quadratic.py
@@ -1,6 +1,10 @@
 def quadratic(first_constant, second_constant, third_constant):
+    roots = []
     discriminant = second_constant**2 - 4 * first_constant * third_constant
     first_root = (-1 * second_constant + discriminant**(1/2)) / (2 * first_constant)
     second_root = (-1 * second_constant - discriminant**(1/2)) / (2 * first_constant)
-    roots = [first_root, second_root]
+    if not isinstance(first_root, complex):
+        roots.append(first_root)
+    if not isinstance(second_root, complex):
+        roots.append(second_root)
     return roots
