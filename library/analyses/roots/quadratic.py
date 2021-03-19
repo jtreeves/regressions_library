e046aa6fa9a7300d4b0d3f0e4f6223d5aa48e26c
jtreeves <jackson.t.reeves@gmail.com>
2021-03-19 14:26:49 -0400

    [roots] Update quadratic to only return one root if both roots are identical

--- library/analyses/roots/quadratic.py
+++ library/analyses/roots/quadratic.py
@@ -3,8 +3,11 @@ def quadratic(first_constant, second_constant, third_constant):
     discriminant = second_constant**2 - 4 * first_constant * third_constant
     first_root = (-1 * second_constant + discriminant**(1/2)) / (2 * first_constant)
     second_root = (-1 * second_constant - discriminant**(1/2)) / (2 * first_constant)
-    if not isinstance(first_root, complex):
+    if first_root == second_root:
         roots.append(first_root)
-    if not isinstance(second_root, complex):
-        roots.append(second_root)
+    else:
+        if not isinstance(first_root, complex):
+            roots.append(first_root)
+        if not isinstance(second_root, complex):
+            roots.append(second_root)
     return roots
