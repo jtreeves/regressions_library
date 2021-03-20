bc22f6783760b519620a796c7c22f51789a6aa69
jtreeves <jackson.t.reeves@gmail.com>
2021-03-20 16:19:01 -0400

    [exponential] Refactor model with new helper functions

--- library/models/exponential.py
+++ library/models/exponential.py
@@ -1,22 +1,14 @@
 from math import log, exp
-from numpy import matrix
-from numpy.linalg import inv
 from library.vectors.dimension import dimension
-from library.vectors.unify import unify
-from library.matrices.multiplication import multiplication
-from library.matrices.transpose import transpose
-from library.matrices.inverse import inverse
+from library.vectors.column import column
+from library.matrices.solve import solve
 from library.analyses.equations.exponential import exponential as exponential_equation
-from library.analyses.roots.exponential import exponential as exponential_roots
 from library.analyses.derivatives.exponential import exponential as exponential_derivative
 from library.analyses.integrals.exponential import exponential as exponential_integral
-from library.analyses.extrema import extrema as extrema_independent
-from library.analyses.inflections import inflections as inflections_independent
+from library.analyses.key_points import key_points
 from library.analyses.accumulation import accumulation
 from library.analyses.mean_values import average_values
-from library.statistics.maximum import maximum
-from library.statistics.minimum import minimum
-from library.statistics.quartiles import quartiles
+from library.statistics.five_number_summary import five_number_summary
 from library.statistics.correlation import correlation
 
 def exponential(data):
@@ -27,57 +19,19 @@ def exponential(data):
     for i in range(len(data)):
         independent_matrix.append([independent_variable[i], 1])
         dependent_matrix.append([log(dependent_variable[i])])
-    transposition = transpose(independent_matrix)
-    product = multiplication(transposition, independent_matrix)
-    product_matrix = matrix(product, dtype='float')
-    inversion = inv(product_matrix)
-    inversion_list = matrix.tolist(inversion)
-    second_product = multiplication(inversion_list, transposition)
-    solution_column = multiplication(second_product, dependent_matrix)
-    solution = dimension(solution_column, 1)
+    solution = solve(independent_matrix, dependent_matrix)
     constants = [exp(solution[1]), exp(solution[0])]
     equation = exponential_equation(*constants)
     derivative = exponential_derivative(*constants)
     integral = exponential_integral(*constants)['evaluation']
     first_derivative = derivative['first']['evaluation']
     second_derivative = derivative['second']['evaluation']
-    roots = exponential_roots(*constants)
-    zeroes = []
-    for i in range(len(roots)):
-        zeroes.append(0)
-    root_coordinates = unify(roots, zeroes)
-    extrema_inputs = extrema_independent('exponential', constants, first_derivative)
-    maxima_inputs = extrema_inputs['maxima']
-    minima_inputs = extrema_inputs['minima']
-    inflections_inputs = inflections_independent('exponential', constants, second_derivative)
-    maxima_outputs = []
-    maxima_coordinates = []
-    minima_outputs = []
-    minima_coordinates = []
-    inflections_outputs = []
-    inflections_coordinates = []
-    if maxima_inputs[0] == None:
-        maxima_coordinates = [None]
-    else:
-        for i in range(len(maxima_inputs)):
-            maxima_outputs.append(equation(maxima_inputs[i]))
-        maxima_coordinates = unify(maxima_inputs, maxima_outputs)
-    if minima_inputs[0] == None:
-        minima_coordinates = [None]
-    else:
-        for i in range(len(minima_inputs)):
-            minima_outputs.append(equation(minima_inputs[i]))
-        minima_coordinates = unify(minima_inputs, minima_outputs)
-    if inflections_inputs[0] == None:
-        inflections_coordinates = [None]
-    else:
-        for i in range(len(inflections_inputs)):
-            inflections_outputs.append(equation(inflections_inputs[i]))
-        inflections_coordinates = unify(inflections_inputs, inflections_outputs)
-    min_value = minimum(independent_variable)
-    max_value = maximum(independent_variable)
-    q1 = quartiles(independent_variable, 1)
-    q3 = quartiles(independent_variable, 3)
+    points = key_points('exponential', solution, equation, first_derivative, second_derivative)
+    five_numbers = five_number_summary(independent_variable)
+    min_value = five_numbers['minimum']
+    max_value = five_numbers['maximum']
+    q1 = five_numbers['q1']
+    q3 = five_numbers['q3']
     accumulated_range = accumulation(integral, min_value, max_value)
     accumulated_iqr = accumulation(integral, q1, q3)
     averages_range = average_values('exponential', equation, integral, min_value, max_value, constants)
@@ -92,10 +46,10 @@ def exponential(data):
         'integral': integral
     }
     points = {
-        'roots': root_coordinates,
-        'maxima': maxima_coordinates,
-        'minima': minima_coordinates,
-        'inflections': inflections_coordinates
+        'roots': points['roots'],
+        'maxima': points['maxima'],
+        'minima': points['minima'],
+        'inflections': points['inflections']
     }
     accumulations = {
         'range': accumulated_range,
