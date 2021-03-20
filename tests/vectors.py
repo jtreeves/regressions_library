000bf87bddc583023256e3bb945a28c196b07599
jtreeves <jackson.t.reeves@gmail.com>
2021-03-20 00:58:56 -0400

    [tests] Ensure all functions from vectors folder have tests

--- tests/vectors.py
+++ tests/vectors.py
@@ -1,32 +1,67 @@
-from library.vectors.addition import addition
-from library.vectors.dimension import dimension
 from library.vectors.direction import direction
-from library.vectors.dot_product import dot_product
 from library.vectors.magnitude import magnitude
-from library.vectors.scalar import scalar
-from library.vectors.unify import unify
 from library.vectors.unit import unit
+from library.vectors.column import column
+from library.vectors.dimension import dimension
+from library.vectors.unify import unify
+from library.vectors.addition import addition
+from library.vectors.scalar import scalar
+from library.vectors.dot_product import dot_product
 
-vec1 = [2, 5, 9, 13]
-vec2 = [1, -7, 23, -2]
-vec3 = [[3, 4], [5, 9], [2, 8]]
+first_point = [2, -3]
+second_point = [5, 7]
 
-vecadd = addition(vec1, vec2)
-vecdim1 = dimension(vec3, 1)
-vecdim2 = dimension(vec3, 2)
-vecunify = unify(vec1, vec2)
-vecdir = direction(vec1, vec2)
-vecdot = dot_product(vec1, vec2)
-vecmag = magnitude(vec1)
-vecscal = scalar(vec1, 5)
-vecunit = unit(vec1)
+first_vector = [2, 5, 9, 13]
+second_vector = [1, -7, 23, -2]
 
-print(f'VECADD: {vecadd}') # => [3, -2, 32, 11]
-print(f'VECDIM1: {vecdim1}') # => [3, 5, 2]
-print(f'VECDIM1: {vecdim2}') # => [4, 9, 8]
-print(f'VECUNIFY: {vecunify}') # => [[2, 1], [5, -7], [9, 23], [13, -2]]
-print(f'VECDIR: {vecdir}') # => {'radian': 1.4876550949064553, 'degree': 85.23635830927383}
-print(f'VECDOT: {vecdot}') # => 148
-print(f'VECMAG: {vecmag}') # => 16.703293088490067
-print(f'VECSCAL: {vecscal}') # => [10, 25, 45, 65]
-print(f'VECUNIT: {vecunit}') # => [0.11973686801784993, 0.2993421700446248, 0.5388159060803247, 0.7782896421160245]
+nested_vector = [
+    [3, 4], 
+    [5, 9], 
+    [2, 8]
+]
+
+scalar_number = -3
+
+direction_vector = direction(first_point, second_point)
+magnitude_vector = magnitude(first_point)
+unit_vector = unit(first_point)
+
+column_first = column(first_vector)
+column_second = column(second_vector)
+
+dimension_first = dimension(nested_vector, 1)
+dimension_second = dimension(nested_vector, 2)
+
+unify_first = unify(first_vector, second_vector)
+unify_second = unify(second_vector, first_vector)
+
+addition_first = addition(first_point, second_point)
+addition_second = addition(first_vector, second_vector)
+
+scalar_first = scalar(first_vector, scalar_number)
+scalar_second = scalar(second_vector, scalar_number)
+
+dot_product_first = dot_product(first_point, second_point)
+dot_product_second = dot_product(first_vector, second_vector)
+
+print(f'DIRECTION VECTOR: {direction_vector}')
+print(f'MAGNITUDE VECTOR: {magnitude_vector}')
+print(f'UNIT VECTOR: {unit_vector}')
+
+print(f'COLUMN FIRST: {column_first}')
+print(f'COLUMN SECOND: {column_second}')
+
+print(f'DIMENSION FIRST: {dimension_first}')
+print(f'DIMENSION SECOND: {dimension_second}')
+
+print(f'UNIFY FIRST: {unify_first}')
+print(f'UNIFY SECOND: {unify_second}')
+
+print(f'ADDITION FIRST: {addition_first}')
+print(f'ADDITION SECOND: {addition_second}')
+
+print(f'SCALAR FIRST: {scalar_first}')
+print(f'SCALAR SECOND: {scalar_second}')
+
+print(f'DOT PRODUCT FIRST: {dot_product_first}')
+print(f'DOT PRODUCT SECOND: {dot_product_second}')
