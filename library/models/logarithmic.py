b82f512a2c03f2554f6699e3707959613835440d
jtreeves <jackson.t.reeves@gmail.com>
2021-03-20 16:35:07 -0400

    [logarithmic] Refactor model to use new helper functions

--- library/models/logarithmic.py
+++ library/models/logarithmic.py
@@ -1,23 +1,14 @@
 from math import log
-from numpy import matrix
-from numpy.linalg import inv
 from library.vectors.dimension import dimension
 from library.vectors.column import column
-from library.vectors.unify import unify
-from library.matrices.multiplication import multiplication
-from library.matrices.transpose import transpose
-from library.matrices.inverse import inverse
+from library.matrices.solve import solve
 from library.analyses.equations.logarithmic import logarithmic as logarithmic_equation
-from library.analyses.roots.logarithmic import logarithmic as logarithmic_roots
 from library.analyses.derivatives.logarithmic import logarithmic as logarithmic_derivative
 from library.analyses.integrals.logarithmic import logarithmic as logarithmic_integral
-from library.analyses.extrema import extrema as extrema_independent
-from library.analyses.inflections import inflections as inflections_independent
+from library.analyses.key_points import key_points
 from library.analyses.accumulation import accumulation
 from library.analyses.mean_values import average_values
-from library.statistics.maximum import maximum
-from library.statistics.minimum import minimum
-from library.statistics.quartiles import quartiles
+from library.statistics.five_number_summary import five_number_summary
 from library.statistics.correlation import correlation
 
 def logarithmic(data):
@@ -27,56 +18,18 @@ def logarithmic(data):
     dependent_matrix = column(dependent_variable)
     for i in range(len(data)):
         independent_matrix.append([log(independent_variable[i]), 1])
-    transposition = transpose(independent_matrix)
-    product = multiplication(transposition, independent_matrix)
-    product_matrix = matrix(product, dtype='float')
-    inversion = inv(product_matrix)
-    inversion_list = matrix.tolist(inversion)
-    second_product = multiplication(inversion_list, transposition)
-    solution_column = multiplication(second_product, dependent_matrix)
-    solution = dimension(solution_column, 1)
-    equation = logarithmic_equation(solution[0], solution[1])
-    derivative = logarithmic_derivative(solution[0], solution[1])
-    integral = logarithmic_integral(solution[0], solution[1])['evaluation']
+    solution = solve(independent_matrix, dependent_matrix)
+    equation = logarithmic_equation(*solution)
+    derivative = logarithmic_derivative(*solution)
+    integral = logarithmic_integral(*solution)['evaluation']
     first_derivative = derivative['first']['evaluation']
     second_derivative = derivative['second']['evaluation']
-    roots = logarithmic_roots(solution[0], solution[1])
-    zeroes = []
-    for i in range(len(roots)):
-        zeroes.append(0)
-    root_coordinates = unify(roots, zeroes)
-    extrema_inputs = extrema_independent('logarithmic', solution, first_derivative)
-    maxima_inputs = extrema_inputs['maxima']
-    minima_inputs = extrema_inputs['minima']
-    inflections_inputs = inflections_independent('logarithmic', solution, second_derivative)
-    maxima_outputs = []
-    maxima_coordinates = []
-    minima_outputs = []
-    minima_coordinates = []
-    inflections_outputs = []
-    inflections_coordinates = []
-    if maxima_inputs[0] == None:
-        maxima_coordinates = [None]
-    else:
-        for i in range(len(maxima_inputs)):
-            maxima_outputs.append(equation(maxima_inputs[i]))
-        maxima_coordinates = unify(maxima_inputs, maxima_outputs)
-    if minima_inputs[0] == None:
-        minima_coordinates = [None]
-    else:
-        for i in range(len(minima_inputs)):
-            minima_outputs.append(equation(minima_inputs[i]))
-        minima_coordinates = unify(minima_inputs, minima_outputs)
-    if inflections_inputs[0] == None:
-        inflections_coordinates = [None]
-    else:
-        for i in range(len(inflections_inputs)):
-            inflections_outputs.append(equation(inflections_inputs[i]))
-        inflections_coordinates = unify(inflections_inputs, inflections_outputs)
-    min_value = minimum(independent_variable)
-    max_value = maximum(independent_variable)
-    q1 = quartiles(independent_variable, 1)
-    q3 = quartiles(independent_variable, 3)
+    points = key_points('logarithmic', solution, equation, first_derivative, second_derivative)
+    five_numbers = five_number_summary(independent_variable)
+    min_value = five_numbers['minimum']
+    max_value = five_numbers['maximum']
+    q1 = five_numbers['q1']
+    q3 = five_numbers['q3']
     accumulated_range = accumulation(integral, min_value, max_value)
     accumulated_iqr = accumulation(integral, q1, q3)
     averages_range = average_values('logarithmic', equation, integral, min_value, max_value, solution)
@@ -91,10 +44,10 @@ def logarithmic(data):
         'integral': integral
     }
     points = {
-        'roots': root_coordinates,
-        'maxima': maxima_coordinates,
-        'minima': minima_coordinates,
-        'inflections': inflections_coordinates
+        'roots': points['roots'],
+        'maxima': points['maxima'],
+        'minima': points['minima'],
+        'inflections': points['inflections']
     }
     accumulations = {
         'range': accumulated_range,
@@ -114,6 +67,8 @@ def logarithmic(data):
     }
     return result
 
-# test_set = [[3, 4], [5, 5], [10, 7], [15, 10]]
-# test_eval = logarithmic(test_set)
-# print(test_eval)
+test_set = [[3, 4], [5, 5], [10, 7], [15, 10]]
+test_eval = logarithmic(test_set)
+print(test_eval)
+
+# {'constants': [3.5554345374844885, -0.36082024840229643], 'evaluations': {'equation': <function logarithmic.<locals>.logarithmic_equation at 0x1089bf820>, 'derivative': <function logarithmic.<locals>.first_derivative at 0x1089bf8b0>, 'integral': <function logarithmic.<locals>.logarithmic_integral at 0x1089bf9d0>}, 'points': {'roots': [[1.1068123741825462, 0]], 'maxima': [None], 'minima': [None], 'inflections': [None]}, 'accumulations': {'range': 85.71123855966133, 'iqr': 63.328232989362206}, 'averages': {'range': {'average_value_derivative': 0.47685426165042805, 'mean_values_derivative': [7.456019214715341], 'average_value_integral': 7.142603213305111, 'mean_values_integral': [8.251621109880897]}, 'iqr': {'average_value_derivative': 0.5341072399844179, 'mean_values_derivative': [6.65678027054682], 'average_value_integral': 6.666129788353916, 'mean_values_integral': [7.216694099878332]}}, 'correlation': 0.9622367701056326}
