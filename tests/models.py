88e1bcde45751fc8fcf528ede156a0ede57a580d
jtreeves <jackson.t.reeves@gmail.com>
2021-03-20 19:10:05 -0400

    [tests] Add sets for each model type to the pertinent test file

---
+++ tests/models.py
@@ -0,0 +1,84 @@
+from library.models.linear import linear
+from library.models.quadratic import quadratic
+from library.models.cubic import cubic
+from library.models.hyperbolic import hyperbolic
+from library.models.exponential import exponential
+from library.models.logarithmic import logarithmic
+
+linear_set = [
+    [1, 30],
+    [2, 27],
+    [3, 24],
+    [4, 21],
+    [5, 18],
+    [6, 15],
+    [7, 12],
+    [8, 9],
+    [9, 6],
+    [10, 3]
+]
+
+quadratic_set = [
+    [1, 10],
+    [2, 27],
+    [3, 40],
+    [4, 49],
+    [5, 54],
+    [6, 55],
+    [7, 52],
+    [8, 45],
+    [9, 34],
+    [10, 19]
+]
+
+cubic_set = [
+    [1, 42],
+    [2, 67],
+    [3, 74],
+    [4, 69],
+    [5, 58],
+    [6, 47],
+    [7, 42],
+    [8, 49],
+    [9, 74],
+    [10, 123]
+]
+
+hyperbolic_set = [
+    [1, 2519],
+    [2, 1259],
+    [3, 839],
+    [4, 629],
+    [5, 503],
+    [6, 419],
+    [7, 359],
+    [8, 314],
+    [9, 279],
+    [10, 251]
+]
+
+exponential_set = [
+    [1, 6],
+    [2, 12],
+    [3, 24],
+    [4, 48],
+    [5, 96],
+    [6, 192],
+    [7, 384],
+    [8, 768],
+    [9, 1536],
+    [10, 3072]
+]
+
+logarithmic_set = [
+    [1, 2],
+    [2, 4.0794],
+    [3, 5.2958],
+    [4, 6.1589],
+    [5, 6.8283],
+    [6, 7.3753],
+    [7, 7.8377],
+    [8, 8.2383],
+    [9, 8.5917],
+    [10, 8.9078]
+]
