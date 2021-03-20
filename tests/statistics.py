2f108172d85429c2d09dbb04ca846d89699cfca9
jtreeves <jackson.t.reeves@gmail.com>
2021-03-20 19:37:08 -0400

    [statistics] Add results of tests as comments

--- tests/statistics.py
+++ tests/statistics.py
@@ -51,39 +51,39 @@ residuals_compare = residuals(first_set, second_set)
 
 correlation_compare = correlation(first_set, second_set)
 
-print(f'SUM FIRST: {sum_first}')
-print(f'SUM SECOND: {sum_second}')
+print(f'SUM FIRST: {sum_first}') # 83
+print(f'SUM SECOND: {sum_second}') # 88
 
-print(f'SORT FIRST: {sort_first}')
-print(f'SORT SECOND: {sort_second}')
+print(f'SORT FIRST: {sort_first}') # [1, 2, 3, 5, 8, 9, 9, 11, 13, 22]
+print(f'SORT SECOND: {sort_second}') # [2, 4, 5, 6, 7, 8, 8, 13, 14, 21]
 
-print(f'MIN FIRST: {min_first}')
-print(f'MIN SECOND: {min_second}')
+print(f'MIN FIRST: {min_first}') # 1
+print(f'MIN SECOND: {min_second}') # 2
 
-print(f'MAX FIRST: {max_first}')
-print(f'MAX SECOND: {max_second}')
+print(f'MAX FIRST: {max_first}') # 22
+print(f'MAX SECOND: {max_second}') # 21
 
-print(f'Q1 FIRST: {q1_first}')
-print(f'Q1 SECOND: {q1_second}')
+print(f'Q1 FIRST: {q1_first}') # 6.5
+print(f'Q1 SECOND: {q1_second}') # 6.5
 
-print(f'Q3 FIRST: {q3_first}')
-print(f'Q3 SECOND: {q3_second}')
+print(f'Q3 FIRST: {q3_first}') # 11
+print(f'Q3 SECOND: {q3_second}') # 13
 
-print(f'MEDIAN FIRST: {median_first}')
-print(f'MEDIAN SECOND: {median_second}')
+print(f'MEDIAN FIRST: {median_first}') # 8.5
+print(f'MEDIAN SECOND: {median_second}') # 7.5
 
-print(f'MEAN FIRST: {mean_first}')
-print(f'MEAN SECOND: {mean_second}')
+print(f'MEAN FIRST: {mean_first}') # 8.3
+print(f'MEAN SECOND: {mean_second}') # 8.8
 
-print(f'FIVE NUMBERS FIRST: {five_first}')
-print(f'FIVE NUMBERS SECOND: {five_second}')
+print(f'FIVE NUMBERS FIRST: {five_first}') # {'minimum': 1, 'q1': 6.5, 'median': 8.5, 'q3': 11, 'maximum': 22}
+print(f'FIVE NUMBERS SECOND: {five_second}') # {'minimum': 2, 'q1': 6.5, 'median': 7.5, 'q3': 13, 'maximum': 21}
 
-print(f'RANGE FIRST: {range_first}')
-print(f'RANGE SECOND: {range_second}')
+print(f'RANGE FIRST: {range_first}') # 21
+print(f'RANGE SECOND: {range_second}') # 19
 
-print(f'DEVIATIONS FIRST: {deviations_first}')
-print(f'DEVIATIONS SECOND: {deviations_second}')
+print(f'DEVIATIONS FIRST: {deviations_first}') # [-0.3000000000000007, -6.300000000000001, -3.3000000000000007, 0.6999999999999993, -7.300000000000001, -5.300000000000001, 13.7, 2.6999999999999993, 0.6999999999999993, 4.699999999999999]
+print(f'DEVIATIONS SECOND: {deviations_second}') # [-1.8000000000000007, -4.800000000000001, -2.8000000000000007, -0.8000000000000007, -6.800000000000001, -3.8000000000000007, 12.2, 5.199999999999999, -0.8000000000000007, 4.199999999999999]
 
-print(f'RESIDUALS COMPARE: {residuals_compare}')
+print(f'RESIDUALS COMPARE: {residuals_compare}') # [1, -2, -1, 1, -1, -2, 1, -3, 1, 0]
 
-print(f'CORRELATION COMPARE: {correlation_compare}')
+print(f'CORRELATION COMPARE: {correlation_compare}') # 0.9665942708463666
