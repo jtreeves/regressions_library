c76dcc8c04fd5cb8bcbbd18e8955b85b057ac99a
jtreeves <jackson.t.reeves@gmail.com>
2021-03-20 16:38:37 -0400

    [quadratic] Refactor model to use new helper functions

--- library/models/quadratic.py
+++ library/models/quadratic.py
@@ -1,22 +1,13 @@
-from numpy import matrix
-from numpy.linalg import inv
 from library.vectors.dimension import dimension
 from library.vectors.column import column
-from library.vectors.unify import unify
-from library.matrices.multiplication import multiplication
-from library.matrices.transpose import transpose
-from library.matrices.inverse import inverse
+from library.matrices.solve import solve
 from library.analyses.equations.quadratic import quadratic as quadratic_equation
-from library.analyses.roots.quadratic import quadratic as quadratic_roots
 from library.analyses.derivatives.quadratic import quadratic as quadratic_derivative
 from library.analyses.integrals.quadratic import quadratic as quadratic_integral
-from library.analyses.extrema import extrema as extrema_independent
-from library.analyses.inflections import inflections as inflections_independent
+from library.analyses.key_points import key_points
 from library.analyses.accumulation import accumulation
 from library.analyses.mean_values import average_values
-from library.statistics.maximum import maximum
-from library.statistics.minimum import minimum
-from library.statistics.quartiles import quartiles
+from library.statistics.five_number_summary import five_number_summary
 from library.statistics.correlation import correlation
 
 def quadratic(data):
@@ -26,56 +17,18 @@ def quadratic(data):
     dependent_matrix = column(dependent_variable)
     for i in range(len(data)):
         independent_matrix.append([independent_variable[i]**2, independent_variable[i], 1])
-    transposition = transpose(independent_matrix)
-    product = multiplication(transposition, independent_matrix)
-    product_matrix = matrix(product, dtype='float')
-    inversion = inv(product_matrix)
-    inversion_list = matrix.tolist(inversion)
-    second_product = multiplication(inversion_list, transposition)
-    solution_column = multiplication(second_product, dependent_matrix)
-    solution = dimension(solution_column, 1)
-    equation = quadratic_equation(solution[0], solution[1], solution[2])
-    derivative = quadratic_derivative(solution[0], solution[1], solution[2])
-    integral = quadratic_integral(solution[0], solution[1], solution[2])['evaluation']
+    solution = solve(independent_matrix, dependent_matrix)
+    equation = quadratic_equation(*solution)
+    derivative = quadratic_derivative(*solution)
+    integral = quadratic_integral(*solution)['evaluation']
     first_derivative = derivative['first']['evaluation']
     second_derivative = derivative['second']['evaluation']
-    roots = quadratic_roots(solution[0], solution[1], solution[2])
-    zeroes = []
-    for i in range(len(roots)):
-        zeroes.append(0)
-    root_coordinates = unify(roots, zeroes)
-    extrema_inputs = extrema_independent('quadratic', solution, first_derivative)
-    maxima_inputs = extrema_inputs['maxima']
-    minima_inputs = extrema_inputs['minima']
-    inflections_inputs = inflections_independent('quadratic', solution, second_derivative)
-    maxima_outputs = []
-    maxima_coordinates = []
-    minima_outputs = []
-    minima_coordinates = []
-    inflections_outputs = []
-    inflections_coordinates = []
-    if maxima_inputs[0] == None:
-        maxima_coordinates = [None]
-    else:
-        for i in range(len(maxima_inputs)):
-            maxima_outputs.append(equation(maxima_inputs[i]))
-        maxima_coordinates = unify(maxima_inputs, maxima_outputs)
-    if minima_inputs[0] == None:
-        minima_coordinates = [None]
-    else:
-        for i in range(len(minima_inputs)):
-            minima_outputs.append(equation(minima_inputs[i]))
-        minima_coordinates = unify(minima_inputs, minima_outputs)
-    if inflections_inputs[0] == None:
-        inflections_coordinates = [None]
-    else:
-        for i in range(len(inflections_inputs)):
-            inflections_outputs.append(equation(inflections_inputs[i]))
-        inflections_coordinates = unify(inflections_inputs, inflections_outputs)
-    min_value = minimum(independent_variable)
-    max_value = maximum(independent_variable)
-    q1 = quartiles(independent_variable, 1)
-    q3 = quartiles(independent_variable, 3)
+    points = key_points('quadratic', solution, equation, first_derivative, second_derivative)
+    five_numbers = five_number_summary(independent_variable)
+    min_value = five_numbers['minimum']
+    max_value = five_numbers['maximum']
+    q1 = five_numbers['q1']
+    q3 = five_numbers['q3']
     accumulated_range = accumulation(integral, min_value, max_value)
     accumulated_iqr = accumulation(integral, q1, q3)
     averages_range = average_values('quadratic', equation, integral, min_value, max_value, solution)
@@ -90,10 +43,10 @@ def quadratic(data):
         'integral': integral
     }
     points = {
-        'roots': root_coordinates,
-        'maxima': maxima_coordinates,
-        'minima': minima_coordinates,
-        'inflections': inflections_coordinates
+        'roots': points['roots'],
+        'maxima': points['maxima'],
+        'minima': points['minima'],
+        'inflections': points['inflections']
     }
     accumulations = {
         'range': accumulated_range,
@@ -113,6 +66,8 @@ def quadratic(data):
     }
     return result
 
-# test_set = [[3, 4], [5, 11], [10, 13], [15, 5]]
-# test_eval = quadratic(test_set)
-# print(test_eval)
+test_set = [[3, 4], [5, 11], [10, 13], [15, 5]]
+test_eval = quadratic(test_set)
+print(test_eval)
+
+# {'constants': [-0.25471134704711385, 4.592932979429327, -6.7813536828135454], 'evaluations': {'equation': <function quadratic.<locals>.quadratic_equation at 0x114ad1790>, 'derivative': <function quadratic.<locals>.first_derivative at 0x114ad1820>, 'integral': <function quadratic.<locals>.quadratic_integral at 0x114ad1940>}, 'points': {'roots': [[1.6224599450221975, 0], [16.40945356285845, 0]], 'maxima': [[9.015956753940323, 13.923488875326996]], 'minima': [None], 'inflections': [None]}, 'accumulations': {'range': 130.40265428002579, 'iqr': 110.19653284671479}, 'averages': {'range': {'average_value_derivative': 0.008128732581278738, 'mean_values_derivative': [9.0], 'average_value_integral': 10.866887856668816, 'mean_values_integral': [5.55181838805534, 12.480095119825306]}, 'iqr': {'average_value_derivative': 0.6449071001990628, 'mean_values_derivative': [7.75], 'average_value_integral': 11.599635036496293, 'mean_values_integral': [5.995446890327815, 12.036466617552831]}}, 'correlation': 0.9793862893911137}
