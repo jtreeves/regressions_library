b0c91ef1b7fda45fa0c1622bb0f808b1c3d83875
jtreeves <jackson.t.reeves@gmail.com>
2021-03-21 20:40:56 -0400

    [tests] Replace manual tests with automated tests by using unittest in statistics file

--- tests/statistics.py
+++ tests/statistics.py
@@ -1,3 +1,5 @@
+import unittest
+
 from library.statistics.rounding import rounding
 from library.statistics.summation import summation
 from library.statistics.sort import sort
@@ -22,86 +24,129 @@ extreme_decimal = 0.000000000000000000005782016894
 precision = 4
 high_precision = 8
 
-round_normal = rounding(normal_decimal, precision)
-round_normal_high = rounding(normal_decimal, high_precision)
-round_extreme = rounding(extreme_decimal, precision)
-round_extreme_high = rounding(extreme_decimal, high_precision)
+class TestRounding(unittest.TestCase):
+    def test_round_normal(self):
+        round_normal = rounding(normal_decimal, precision)
+        self.assertEqual(round_normal, 6.8172)
 
-sum_even = summation(even_set)
-sum_odd = summation(odd_set)
+    def test_round_normal_high(self):
+        round_normal_high = rounding(normal_decimal, high_precision)
+        self.assertEqual(round_normal_high, 6.81723983)
+    
+    def test_round_extreme(self):
+        round_extreme = rounding(extreme_decimal, precision)
+        self.assertEqual(round_extreme, 0.0001)
+    
+    def test_round_extreme_high(self):
+        round_extreme_high = rounding(extreme_decimal, high_precision) 
+        self.assertEqual(round_extreme_high, 1e-08)
 
-sort_even = sort(even_set)
-sort_odd = sort(odd_set)
+class TestSummation(unittest.TestCase):
+    def test_sum_even(self):
+        sum_even = summation(even_set)
+        self.assertEqual(sum_even, 83)
 
-min_even = minimum(even_set)
-min_odd = minimum(odd_set)
+    def test_sum_odd(self):
+        sum_odd = summation(odd_set)
+        self.assertEqual(sum_odd, 79)
 
-max_even = maximum(even_set)
-max_odd = maximum(odd_set)
+class TestSort(unittest.TestCase):
+    def test_sort_even(self):
+        sort_even = sort(even_set)
+        self.assertEqual(sort_even, [1, 2, 3, 5, 8, 9, 9, 11, 13, 22])
 
-q1_even = quartiles(even_set, 1)
-q1_odd = quartiles(odd_set, 1)
+    def test_sort_odd(self):
+        sort_odd = sort(odd_set)
+        self.assertEqual(sort_odd, [2, 4, 5, 6, 7, 8, 8, 14, 25])
 
-q3_even = quartiles(even_set, 3)
-q3_odd = quartiles(odd_set, 3)
+class TestMinimum(unittest.TestCase):
+    def test_min_even(self):
+        min_even = minimum(even_set)
+        self.assertEqual(min_even, 1)
 
-median_even = median(even_set)
-median_odd = median(odd_set)
+    def test_min_odd(self):
+        min_odd = minimum(odd_set)
+        self.assertEqual(min_odd, 2)
 
-mean_even = mean(even_set)
-mean_odd = mean(odd_set)
+class TestMaximum(unittest.TestCase):
+    def test_max_even(self):
+        max_even = maximum(even_set)
+        self.assertEqual(max_even, 22)
 
-five_even = five_number_summary(even_set, precision)
-five_odd = five_number_summary(odd_set, precision)
+    def test_max_odd(self):
+        max_odd = maximum(odd_set)
+        self.assertEqual(max_odd, 25)
 
-range_even = ranges(even_set)
-range_odd = ranges(odd_set)
+class TestQuartiles(unittest.TestCase):
+    def test_q1_even(self):
+        q1_even = quartiles(even_set, 1)
+        self.assertEqual(q1_even, 3)
+    
+    def test_q1_odd(self):
+        q1_odd = quartiles(odd_set, 1)
+        self.assertEqual(q1_odd, 4.5)
+    
+    def test_q3_even(self):
+        q3_even = quartiles(even_set, 3)
+        self.assertEqual(q3_even, 11)
+    
+    def test_q3_odd(self):
+        q3_odd = quartiles(odd_set, 3)
+        self.assertEqual(q3_odd, 11)
 
-deviations_even = deviations(even_set)
-deviations_odd = deviations(odd_set)
+class TestMedian(unittest.TestCase):
+    def test_median_even(self):
+        median_even = median(even_set)
+        self.assertEqual(median_even, 8.5)
+    
+    def test_median_odd(self):
+        median_odd = median(odd_set)
+        self.assertEqual(median_odd, 7)
 
-residuals_compare = residuals(odd_set, compare_set)
+class TestMean(unittest.TestCase):
+    def test_mean_even(self):
+        mean_even = mean(even_set)
+        self.assertEqual(mean_even, 8.3)
+    
+    def test_mean_odd(self):
+        mean_odd = mean(odd_set)
+        self.assertAlmostEqual(mean_odd, 8.7778, 4)
 
-correlation_compare = correlation(odd_set, compare_set, precision)
+class TestFiveNumberSummary(unittest.TestCase):
+    def test_five_even(self):
+        five_even = five_number_summary(even_set, precision)
+        self.assertEqual(five_even, {'minimum': 1, 'q1': 3, 'median': 8.5, 'q3': 11, 'maximum': 22})
+    
+    def test_five_odd(self):
+        five_odd = five_number_summary(odd_set, precision)
+        self.assertEqual(five_odd, {'minimum': 2, 'q1': 4.5, 'median': 7, 'q3': 11.0, 'maximum': 25})
 
-print(f'ROUND NORMAL: {round_normal}') # 6.8172
-print(f'ROUND NORMAL HIGH: {round_normal_high}') # 6.81723983
-print(f'ROUND EXTREME: {round_extreme}') # 0.0001
-print(f'ROUND EXTREME HIGH: {round_extreme_high}') # 1e-08
+class TestRange(unittest.TestCase):
+    def test_range_even(self):
+        range_even = ranges(even_set)
+        self.assertEqual(range_even, 21)
+    
+    def test_range_odd(self):
+        range_odd = ranges(odd_set)
+        self.assertEqual(range_odd, 23)
 
-print(f'SUM EVEN: {sum_even}') # 83
-print(f'SUM ODD: {sum_odd}') # 79
+class TestDeviations(unittest.TestCase):
+    def test_deviations_even(self):
+        deviations_even = deviations(even_set)
+        self.assertAlmostEqual(deviations_even[0], -0.3)
+    
+    def test_deviations_odd(self):
+        deviations_odd = deviations(odd_set)
+        self.assertAlmostEqual(deviations_odd[0], -1.7778, 4)
 
-print(f'SORT EVEN: {sort_even}') # [1, 2, 3, 5, 8, 9, 9, 11, 13, 22]
-print(f'SORT ODD: {sort_odd}') # [2, 4, 5, 6, 7, 8, 8, 14, 25]
+class TestComparisons(unittest.TestCase):
+    def test_residuals_compare(self):
+        residuals_compare = residuals(odd_set, compare_set)
+        self.assertEqual(residuals_compare, [2, -1, 1, -2, 1, -2, 3, 1, 0])
+    
+    def test_correlation_compare(self):
+        correlation_compare = correlation(odd_set, compare_set, precision)
+        self.assertEqual(correlation_compare, 0.967)
 
-print(f'MIN EVEN: {min_even}') # 1
-print(f'MIN ODD: {min_odd}') # 2
-
-print(f'MAX EVEN: {max_even}') # 22
-print(f'MAX ODD: {max_odd}') # 25
-
-print(f'Q1 EVEN: {q1_even}') # 3
-print(f'Q1 ODD: {q1_odd}') # 4.5
-
-print(f'Q3 EVEN: {q3_even}') # 11
-print(f'Q3 ODD: {q3_odd}') # 11.0
-
-print(f'MEDIAN EVEN: {median_even}') # 8.5
-print(f'MEDIAN ODD: {median_odd}') # 7
-
-print(f'MEAN EVEN: {mean_even}') # 8.3
-print(f'MEAN ODD: {mean_odd}') # 8.777777777777779
-
-print(f'FIVE NUMBERS EVEN: {five_even}') # {'minimum': 1, 'q1': 3, 'median': 8.5, 'q3': 11, 'maximum': 22}
-print(f'FIVE NUMBERS ODD: {five_odd}') # {'minimum': 2, 'q1': 4.5, 'median': 7, 'q3': 11.0, 'maximum': 25}
-
-print(f'RANGE EVEN: {range_even}') # 21
-print(f'RANGE ODD: {range_odd}') # 23
-
-print(f'DEVIATIONS EVEN: {deviations_even}') # [-0.3000000000000007, -6.300000000000001, -3.3000000000000007, 0.6999999999999993, -7.300000000000001, -5.300000000000001, 13.7, 2.6999999999999993, 0.6999999999999993, 4.699999999999999]
-print(f'DEVIATIONS ODD: {deviations_odd}') # [-1.7777777777777786, -4.777777777777779, -2.7777777777777786, -0.7777777777777786, -6.777777777777779, -3.7777777777777786, 16.22222222222222, 5.222222222222221, -0.7777777777777786]
-
-print(f'RESIDUALS COMPARE: {residuals_compare}') # [2, -1, 1, -2, 1, -2, 3, 1, 0]
-
-print(f'CORRELATION COMPARE: {correlation_compare}') # 0.967
+if __name__ == '__main__':
+    unittest.main()
