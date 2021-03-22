dfae01c8f64059684a625c177b5ddfad32da00a6
jtreeves <jackson.t.reeves@gmail.com>
2021-03-21 20:15:33 -0400

    [tests] Encapsulate initial function calls in pertinent classes where applicable

--- tests/vectors.py
+++ tests/vectors.py
@@ -26,81 +26,75 @@ nested_vector = [
 scalar_number = -3
 
 component_vector = component_form(first_point, second_point)
-direction_vector = direction(component_vector)
-magnitude_vector = magnitude(component_vector)
-unit_vector = unit(component_vector)
 
 class TestSimpleVector(unittest.TestCase):
     def test_component_vector(self):
         self.assertEqual(component_vector, [3, 10])
 
     def test_direction_vector(self):
+        direction_vector = direction(component_vector)
         self.assertAlmostEqual(direction_vector['degree'], 73.3008, 4)
     
     def test_magnitude_vector(self):
+        magnitude_vector = magnitude(component_vector)
         self.assertAlmostEqual(magnitude_vector, 10.4403, 4)
     
     def test_unit_vector(self):
+        unit_vector = unit(component_vector)
         self.assertAlmostEqual(unit_vector[0], 0.2873, 4)
-
-column_first = column(first_vector)
-column_second = column(second_vector)
 
 class TestColumn(unittest.TestCase):
     def test_column_first(self):
+        column_first = column(first_vector)
         self.assertEqual(column_first, [[2], [5], [9], [13]])
 
     def test_column_second(self):
+        column_second = column(second_vector)
         self.assertEqual(column_second, [[1], [-7], [23], [-2]])
-
-dimension_first = dimension(nested_vector, 1)
-dimension_second = dimension(nested_vector, 2)
 
 class TestDimension(unittest.TestCase):
     def test_dimension_first(self):
+        dimension_first = dimension(nested_vector, 1)
         self.assertEqual(dimension_first, [3, 5, 2])
 
     def test_dimension_second(self):
+        dimension_second = dimension(nested_vector, 2)
         self.assertEqual(dimension_second, [4, 9, 8])
-
-unify_first = unify(first_vector, second_vector)
-unify_second = unify(second_vector, first_vector)
 
 class TestUnify(unittest.TestCase):
     def test_unify_first(self):
+        unify_first = unify(first_vector, second_vector)
         self.assertEqual(unify_first, [[2, 1], [5, -7], [9, 23], [13, -2]])
 
     def test_unify_second(self):
+        unify_second = unify(second_vector, first_vector)
         self.assertEqual(unify_second, [[1, 2], [-7, 5], [23, 9], [-2, 13]])
-
-addition_first = addition(first_point, second_point)
-addition_second = addition(first_vector, second_vector)
 
 class TestAddition(unittest.TestCase):
     def test_addition_first(self):
+        addition_first = addition(first_point, second_point)
         self.assertEqual(addition_first, [7, 4])
 
     def test_addition_second(self):
+        addition_second = addition(first_vector, second_vector)
         self.assertEqual(addition_second, [3, -2, 32, 11])
-
-scalar_first = scalar(first_vector, scalar_number)
-scalar_second = scalar(second_vector, scalar_number)
 
 class TestScalar(unittest.TestCase):
     def test_scalar_first(self):
+        scalar_first = scalar(first_vector, scalar_number)
         self.assertEqual(scalar_first, [-6, -15, -27, -39])
 
     def test_scalar_second(self):
+        scalar_second = scalar(second_vector, scalar_number)
         self.assertEqual(scalar_second, [-3, 21, -69, 6])
-
-dot_product_first = dot_product(first_point, second_point)
-dot_product_second = dot_product(first_vector, second_vector)
 
 class TestDotProduct(unittest.TestCase):
     def test_dot_product_first(self):
+        dot_product_first = dot_product(first_point, second_point)
         self.assertEqual(dot_product_first, -11)
 
     def test_dot_product_second(self):
+        dot_product_second = dot_product(first_vector, second_vector)
         self.assertEqual(dot_product_second, 148)
 
 if __name__ == '__main__':
