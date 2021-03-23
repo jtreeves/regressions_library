aeecf50a97048d7aaa79d947a3d792a69b7c35f5
jtreeves <jackson.t.reeves@gmail.com>
2021-03-22 21:43:03 -0400

    [sinusoidal] Create function to generate sinusoidal regressions using scipy

---
+++ library/models/sinusoidal.py
@@ -0,0 +1,12 @@
+from numpy import sin
+from scipy.optimize import curve_fit
+
+def sinusoidal_function(variable, a, b, c, d):
+    return a * sin(b * (variable - c)) + d
+
+inputs = [2.0, 3.0, 5.0, 7.0, 11.0, 13.0, 17.0, 19.0]
+outputs = [5.0, 6.0, 8.0, 7.0, 4.0, 3.0, 1.0, 3.0]
+
+parameters, parameters_covariance = curve_fit(sinusoidal_function, inputs, outputs)
+
+print(parameters)
