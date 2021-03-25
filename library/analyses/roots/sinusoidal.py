a86f2a5145b496382dec0eaff6ef360eb1583759
jtreeves <jackson.t.reeves@gmail.com>
2021-03-25 11:59:04 -0400

    [sinusoidal] Round numbers to be stringified within general forms

--- library/analyses/roots/sinusoidal.py
+++ library/analyses/roots/sinusoidal.py
@@ -17,25 +17,32 @@ def sinusoidal(first_constant, second_constant, third_constant, fourth_constant,
             second_value = initial_value + 2 * periodic_unit
             third_value = initial_value + 3 * periodic_unit
             fourth_value = initial_value + 4 * periodic_unit
-            general_form = str(initial_value) + ' + ' + str(periodic_unit) + 'k'
+            rounded_initial_value = rounding(initial_value, precision)
+            rounded_periodic_unit = rounding(periodic_unit, precision)
+            general_form = str(rounded_initial_value) + ' + ' + str(rounded_periodic_unit) + 'k'
             roots = [initial_value, first_value, second_value, third_value, fourth_value, general_form]
         elif ratio == 1 or ratio == -1:
             periodic_unit = 2 * pi / second_constant
             initial_value = third_constant + periodic_radians
             first_value = initial_value + 1 * periodic_unit
             second_value = initial_value + 2 * periodic_unit
-            general_form = str(initial_value) + ' + ' + str(periodic_unit) + 'k'
+            rounded_initial_value = rounding(initial_value, precision)
+            rounded_periodic_unit = rounding(periodic_unit, precision)
+            general_form = str(rounded_initial_value) + ' + ' + str(rounded_periodic_unit) + 'k'
             roots = [initial_value, first_value, second_value, general_form]
         else:
             periodic_unit = 2 * pi / second_constant
             initial_value = third_constant + periodic_radians
             first_value = initial_value + 1 * periodic_unit
             second_value = initial_value + 2 * periodic_unit
-            general_form = str(initial_value) + ' + ' + str(periodic_unit) + 'k'
+            rounded_initial_value = rounding(initial_value, precision)
+            rounded_periodic_unit = rounding(periodic_unit, precision)
+            general_form = str(rounded_initial_value) + ' + ' + str(rounded_periodic_unit) + 'k'
             alternative_initial_value = third_constant + pi / second_constant - periodic_radians
             alternative_first_value = alternative_initial_value + 1 * periodic_unit
             alternative_second_value = alternative_initial_value + 2 * periodic_unit
-            alternative_general_form = str(alternative_initial_value) + ' + ' + str(periodic_unit) + 'k'
+            rounded_alternative_initial_value = rounding(alternative_initial_value, precision)
+            alternative_general_form = str(rounded_alternative_initial_value) + ' + ' + str(rounded_periodic_unit) + 'k'
             roots = [initial_value, first_value, second_value, alternative_initial_value, alternative_first_value, alternative_second_value, general_form, alternative_general_form]
     if not roots:
         roots = [None]
