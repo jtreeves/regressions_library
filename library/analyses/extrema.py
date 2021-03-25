cf0f3f3f0af60bc823214e17f061bfef401d0153
jtreeves <jackson.t.reeves@gmail.com>
2021-03-25 16:26:15 -0400

    [extrema, key_points] Preserve part of general form in extrema function; provide general form for maxima and minima in key_points function

--- library/analyses/extrema.py
+++ library/analyses/extrema.py
@@ -5,11 +5,30 @@ from .minima import minima
 
 def extrema(equation_type, coefficients, derivative, precision):
     points = critical_points(equation_type, 1, coefficients, precision)
+    print(f'EXTREMA POINTS: {points}')
     intervals_set = intervals(derivative, points)
-    max_points = maxima(intervals_set)
-    min_points = minima(intervals_set)
-    result = {
-        'maxima': max_points,
-        'minima': min_points
-    }
+    print(f'INTERVALS SET: {intervals_set}')
+    result = {}
+    if equation_type == 'sinusoidal':
+        general_form = intervals_set[-1]
+        print(f'GENERAL FORM: {general_form}')
+        periodic_unit_index = general_form.find(' + ') + 3
+        print(f'PERIODIC UNIT INDEX: {periodic_unit_index}')
+        periodic_unit = general_form[periodic_unit_index:]
+        print(f'PERIODIC UNIT: {periodic_unit}')
+        max_points = maxima(intervals_set)
+        min_points = minima(intervals_set)
+        max_extended = max_points + [periodic_unit]
+        min_extended = min_points + [periodic_unit]
+        result = {
+            'maxima': max_extended,
+            'minima': min_extended
+        }
+    else:
+        max_points = maxima(intervals_set)
+        min_points = minima(intervals_set)
+        result = {
+            'maxima': max_points,
+            'minima': min_points
+        }
     return result
--- library/analyses/key_points.py
+++ library/analyses/key_points.py
@@ -10,6 +10,10 @@ def key_points(equation_type, coefficients, equation, first_derivative, second_d
     maxima_inputs = extrema_inputs['maxima']
     minima_inputs = extrema_inputs['minima']
     inflections_inputs = inflections(equation_type, coefficients, second_derivative, precision)
+    print(f'INTERCEPT INPUTS: {intercepts_inputs}')
+    print(f'MAXIMA INPUTS: {maxima_inputs}')
+    print(f'MINIMA INPUTS: {minima_inputs}')
+    print(f'INFLECTIONS INPUTS: {inflections_inputs}')
     intercepts_outputs = []
     maxima_outputs = []
     minima_outputs = []
@@ -28,17 +32,31 @@ def key_points(equation_type, coefficients, equation, first_derivative, second_d
         maxima_coordinates = [None]
     else:
         for i in range(len(maxima_inputs)):
-            output = equation(maxima_inputs[i])
-            rounded_output = rounding(output, precision)
-            maxima_outputs.append(rounded_output)
+            if isinstance(maxima_inputs[i], (int, float)):
+                output = equation(maxima_inputs[i])
+                rounded_output = rounding(output, precision)
+                maxima_outputs.append(rounded_output)
+            else:
+                periodic_unit = 2 * float(maxima_inputs[i][:-1])
+                initial_value = maxima_inputs[0]
+                general_form = str(initial_value) + ' + ' + str(periodic_unit) + 'k'
+                maxima_inputs[i] = general_form
+                maxima_outputs.append(maxima_outputs[0])
         maxima_coordinates = unify(maxima_inputs, maxima_outputs)
     if minima_inputs[0] == None:
         minima_coordinates = [None]
     else:
         for i in range(len(minima_inputs)):
-            output = equation(minima_inputs[i])
-            rounded_output = rounding(output, precision)
-            minima_outputs.append(rounded_output)
+            if isinstance(minima_inputs[i], (int, float)):
+                output = equation(minima_inputs[i])
+                rounded_output = rounding(output, precision)
+                minima_outputs.append(rounded_output)
+            else:
+                periodic_unit = 2 * float(minima_inputs[i][:-1])
+                initial_value = minima_inputs[0]
+                general_form = str(initial_value) + ' + ' + str(periodic_unit) + 'k'
+                minima_inputs[i] = general_form
+                minima_outputs.append(minima_outputs[0])
         minima_coordinates = unify(minima_inputs, minima_outputs)
     if inflections_inputs[0] == None:
         inflections_coordinates = [None]
