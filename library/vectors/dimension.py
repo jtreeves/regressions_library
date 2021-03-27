04cf063e3930acf412cbeb99b79b8fde9524fb27
jtreeves <jackson.t.reeves@gmail.com>
2021-03-27 14:44:41 -0400

    [vectors/dimension] Add error handling to ensure vector is 2-dimensional and level is integer less than length of inner lists

--- library/vectors/dimension.py
+++ library/vectors/dimension.py
@@ -1,4 +1,10 @@
 def single_dimension(vector, level):
+    if not isinstance(vector, (list, tuple)) or not isinstance(vector[0], (list, tuple)) or isinstance(vector[0][0], (list, tuple)):
+        raise TypeError("First argument must be a 2-dimensional list or tuple")
+    if not isinstance(vector[0][0], (int, float)):
+        raise TypeError("Elements nested within first argument must be integers or floats")
+    if not isinstance(level, int) or not level <= len(vector[0]):
+        raise ValueError("Second argument must be an integer less than or equal to the length of the nested lists")
     result = []
     for i in range(len(vector)):
         result.append(vector[i][level - 1])
