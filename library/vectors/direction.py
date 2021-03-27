ae201118e8257d9f98a171077941a392e9d1ea97
jtreeves <jackson.t.reeves@gmail.com>
2021-03-27 14:53:14 -0400

    [vectors/direction] Add error handling to ensure vector is 1-dimensional with only 2 elements, both of which are numbers

--- library/vectors/direction.py
+++ library/vectors/direction.py
@@ -1,6 +1,12 @@
 from math import atan, degrees
 
 def vector_direction(vector):
+    if not isinstance(vector, (list, tuple)) or isinstance(vector[0], (list, tuple)):
+        raise TypeError("Argument must be a 1-dimensional list or tuple")
+    if not len(vector) == 2:
+        raise ValueError("Argument must contain exactly two elements")
+    if not isinstance(vector[0], (int, float)) or not isinstance(vector[1], (int, float)):
+        raise TypeError("Elements of argument must be integers or floats")
     ratio = vector[1] / vector[0]
     radian_measure = atan(ratio)
     degree_measure = degrees(radian_measure)
