6f4b2109a4b10c8c58f34e3e8ed6c239ce5d47e5
jtreeves <jackson.t.reeves@gmail.com>
2021-03-27 14:27:37 -0400

    [vectors/column] Add error handling to ensure input is a 1-dimensional list that contains numbers

--- library/vectors/column.py
+++ library/vectors/column.py
@@ -1,4 +1,8 @@
 def column_conversion(vector):
+    if not isinstance(vector, (list, tuple)) or isinstance(vector[0], (list, tuple)):
+        raise TypeError("Argument must be a 1-dimensional list or tuple")
+    if not isinstance(vector[0], (int, float)):
+        raise TypeError("Elements of argument must be integers or floats")
     result = []
     for i in vector:
         result.append([i])
