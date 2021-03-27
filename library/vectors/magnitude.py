24e7625b57b3ba84cb89beedc7380d7c286d2c29
jtreeves <jackson.t.reeves@gmail.com>
2021-03-27 14:54:45 -0400

    [vectors/magnitude] Add error handling to ensure vector is 1-dimensional and contains numbers

--- library/vectors/magnitude.py
+++ library/vectors/magnitude.py
@@ -1,6 +1,10 @@
 from library.statistics.summation import sum_value
 
 def vector_magnitude(vector):
+    if not isinstance(vector, (list, tuple)) or isinstance(vector[0], (list, tuple)):
+        raise TypeError("Argument must be a 1-dimensional list or tuple")
+    if not isinstance(vector[0], (int, float)):
+        raise TypeError("Elements of argument must be integers or floats")
     squares = []
     for i in range(len(vector)):
         squares.append(vector[i]**2)
