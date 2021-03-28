3b7b342a10b53c1d6ee2330419c6bc27570ed542
jtreeves <jackson.t.reeves@gmail.com>
2021-03-28 12:12:48 -0400

    [tests] Update runtimes after reconfirming that no tests fail or produce errors

--- library/statistics/halve.py
+++ library/statistics/halve.py
@@ -1,5 +1,6 @@
 from math import floor
 from library.errors.vectors import vector_of_scalars
+from library.errors.scalars import positive_integer
 from library.errors.matrices import matrix_of_scalars
 from .sort import sorted_list, sorted_dimension
 
@@ -29,6 +30,7 @@ def half(data):
 
 def half_dimension(data, dimension):
     matrix_of_scalars(data, 'first')
+    positive_integer(dimension)
     sorted_data = sorted_dimension(data, dimension)
     result = partition(sorted_data)
     return result
--- tests/matrices.py
+++ tests/matrices.py
@@ -209,4 +209,4 @@ class TestSolveSystems(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
-# ---------- Ran 32 tests in 0.012s ---------- OK ---------- #
+# ---------- Ran 32 tests in 0.010s ---------- OK ---------- #
