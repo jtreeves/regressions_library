7821002a69eacc8ba3ad4b3039355d3518a79898
jtreeves <jackson.t.reeves@gmail.com>
2021-03-31 17:42:41 -0400

    [documentation] Use Sphinx to generate documentation from docstrings in library

--- library/analyses/derivatives/cubic.py
+++ library/analyses/derivatives/cubic.py
@@ -1,6 +1,49 @@
 from library.errors.scalars import four_scalars
 
 def cubic_derivatives(first_constant, second_constant, third_constant, fourth_constant):
+    """
+    Calculates first and second derivatives of cubic function
+
+    Parameters
+    ----------
+    first_constant : int or float
+        Coefficient of the cubic term
+    second_constant : int or float
+        Coefficient of the quadratic term
+    third_constant : int or float
+        Coefficient of the linear term
+    fourth_constant : int or float
+        Coefficient of the constant term
+
+    Raises
+    ------
+    TypeError
+        Arguments must be integers or floats
+
+    Returns
+    -------
+    derivatives['first']['constants'] : list
+        Coefficients of first derivative
+    derivatives['first']['evaluation'] : function
+        Function for evaluating first derivative at any float argument
+    derivatives['second']['constants'] : list
+        Coefficients of second derivative
+    derivatives['second']['evaluation'] : function
+        Function for evaluating second derivative at any float argument
+
+    Examples
+    --------
+    Print coefficients of first derivative
+        >>> cbdr = cubic_derivatives(2, 3, 5, 7)
+        >>> print(cbdr['first']['constants'])
+        [6, 6, 5]
+    Evaluate second derivative with an input of 10
+        >>> cbdr = cubic_derivatives(2, 3, 5, 7)
+        >>> scnd = cbdr['second']['evaluation']
+        >>> eval = scnd(10)
+        >>> print(eval)
+        126
+    """
     four_scalars(first_constant, second_constant, third_constant, fourth_constant)
     first_constants = [3 * first_constant, 2 * second_constant, third_constant]
     def first_derivative(variable):
