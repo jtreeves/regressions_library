e89ac1baee9b434f1bef165e979ac3348bb6135b
jtreeves <jackson.t.reeves@gmail.com>
2021-04-07 00:27:08 -0400

    [models/cubic] Add docstring

--- library/models/cubic.py
+++ library/models/cubic.py
@@ -14,6 +14,66 @@ from library.statistics.summary import five_number_summary
 from library.statistics.correlation import correlation_coefficient
 
 def cubic_model(data, precision):
+    """
+    Generates a cubic regression model from data sets
+
+    Parameters
+    ----------
+    data : list or tuple
+        List of lists of numbers representing a collection of coordinate pairs
+    precision : int
+        Maximum number of digits that can appear after the decimal place of the results
+
+    Raises
+    ------
+    TypeError
+        First argument must be a 2-dimensional list or tuple
+    TypeError
+        Elements nested within first argument must be integers or floats
+    ValueError
+        First argument must contain at least 10 elements
+    ValueError
+        Last argument must be a positive integer
+
+    Returns
+    -------
+    model['constants'] : list
+    model['evaluations']['equation'] : function
+    model['evaluations']['derivative'] : function
+    model['evaluations']['integral'] : function
+    model['points']['roots'] : list
+    model['points']['maxima'] : list
+    model['points']['minima'] : list
+    model['points']['inflections'] : list
+    model['accumulations']['range'] : float
+    model['accumulations']['iqr'] : float
+    model['averages']['range'] : dict
+    model['averages']['iqr'] : dict
+    model['correlation'] : float
+
+    Examples
+    --------
+    Generate a cubic regression model for the data set [[1, 42], [2, 67], [3, 74], [4, 69], [5, 58], [6, 47], [7, 42], [8, 49], [9, 74], [10, 123]], then print its coefficients, roots, total accumulation over its interquartile range, and correlation (and round the results to four decimal places)
+        >>> model_perfect = cubic_model([[1, 42], [2, 67], [3, 74], [4, 69], [5, 58], [6, 47], [7, 42], [8, 49], [9, 74], [10, 123]], 4)
+        >>> print(model_perfect['constants'])
+        [1.0, -15.0, 63.0, -7.0]
+        >>> print(model_perfect['points']['roots'])
+        [[0.1142, 0]]
+        >>> print(model_perfect['accumulations']['iqr'])
+        276.25
+        >>> print(model_perfect['correlation'])
+        1.0
+    Generate a cubic regression model for the data set [[1, 32], [2, 25], [3, 14], [4, 23], [5, 39], [6, 45], [7, 42], [8, 49], [9, 36], [10, 33]], then print its coefficients, inflections, total accumulation over its range, and correlation (and round the results to four decimal places)
+        >>> model_agnostic = cubic_model([[1, 32], [2, 25], [3, 14], [4, 23], [5, 39], [6, 45], [7, 42], [8, 49], [9, 36], [10, 33]], 4)
+        >>> print(model_agnostic['constants'])
+        [-0.3881, 6.0932, -24.155, 49.4667]
+        >>> print(model_agnostic['points']['inflections'])
+        [[5.2334, 34.3091]]
+        >>> print(model_agnostic['accumulations']['range'])
+        308.4104
+        >>> print(model_agnostic['correlation'])
+        0.8933
+    """
     matrix_of_scalars(data, 'first')
     long_vector(data)
     positive_integer(precision)
