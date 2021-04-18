0ee7ff9b018cdc4331780e48e63d7b4ca879cb7b
jtreeves <jackson.t.reeves@gmail.com>
2021-04-18 09:16:51 -0400

    [separate] Add docstring to function

--- library/vectors/separate.py
+++ library/vectors/separate.py
@@ -1,6 +1,55 @@
 from library.errors.vectors import allow_none_vector
 
 def separate_elements(vector):
+    """
+    Separates a single vector into two vectors based their data types (numerical and non-numerical)
+
+    Parameters
+    ----------
+    vector : list
+        List containing elements of various types: integers, floats, strings, or None
+
+    Raises
+    ------
+    TypeError
+        Elements of argument must be integers, floats, strings, or None
+
+    Returns
+    -------
+    separate_vectors['numerical'] : list
+        List containing only the integer or float elements from the input vector; may be an empty list if input vector contained no integer or float elements
+    separate_vectors['other'] : list
+        List containing only the string or None elements from the input vector; may be an empty list if input vector contained no string elements and did not merely contain None
+
+    See Also
+    --------
+    :func:`~library.vectors.generate_generate_elements`, :func:`~library.vectors.unify.unite_vectors`
+
+    Notes
+    -----
+    - Set of all real numbers: :math:`\\mathbb{R}`
+    - Set of all strings: :math:`\\mathbb{S}`
+    - Set containing None: :math:`\\{ \\emptyset \\}`
+    - Set of all real numbers, all strings, and None: :math:`\\mathbb{A} = \\{ \\mathbb{R}, \\mathbb{S}, \\emptyset \\}`
+    - Set of mixed elements: :math:`M = \\{ m \\mid m \\in \\mathbb{A} \\}`
+    - Set of numerical elements from mixed elements: :math:`N = \\{ n \\mid n \\in M, n \\in \\mathbb{R} \\}`
+    - Set of non-numerical elements from mixed elements: :math:`O = \\{ o \\mid o \\in M, o \\in \\mathbb{S} \\cup \\emptyset \\}`
+
+    Examples
+    --------
+    Separate [1, 'two', 3, 'four'] into two vectors based on its data types
+        >>> separate_vectors_mixed = separate_elements([1, 'two', 3, 'four'])
+        >>> print(separate_vectors_mixed['numerical'])
+        [1, 3]
+        >>> print(separate_vectors_mixed['other'])
+        ['two', 'four']
+    Separate [None] into two vectors based on its data types
+        >>> separate_vectors_none = separate_elements([None])
+        >>> print(separate_vectors_none['numerical'])
+        []
+        >>> print(separate_vectors_none['other'])
+        [None]
+    """
     # Handle input errors
     allow_none_vector(vector)
 
