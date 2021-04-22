4f098ebd57143686f03f6b5ebb96803325b170a4
jtreeves <jackson.t.reeves@gmail.com>
2021-04-22 16:19:52 -0400

    [setup] Update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='regressions',
-    version='0.0.3',
+    version='2.0.6',
     url='https://github.com/jtreeves/regressions_library',
     license='MIT',
     author='Jackson Reeves',
