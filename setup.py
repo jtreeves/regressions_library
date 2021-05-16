a895caf7276b928cadb5e6cf03a7128e0889e2fb
jtreeves <jackson.t.reeves@gmail.com>
2021-05-16 16:06:30 -0400

    [setup] Increment version number to 2.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='regressions',
-    version='2.0.8',
+    version='2.1.0',
     url='https://github.com/jtreeves/regressions_library',
     license='MIT',
     author='Jackson Reeves',
